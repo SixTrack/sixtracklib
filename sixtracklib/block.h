1ce1bc561a85b747fbf439422bdb17fd99023569
Hussain Jasim <hussain.jasim@mail.utoronto.ca>
2017-03-01 23:03:27 -0500

    Added declarations for the block handling functions.

--- sixtracklib/block.h
+++ sixtracklib/block.h
@@ -42,5 +42,14 @@ int Block_track(value_t *data, Beam *beam,
 
 #endif
 
+// declarations for block handling functions
+CLGLOBAL block_t* block_initialize(unsigned int size);
+CLGLOBAL block_t* block_reshape(CLGLOBAL block_t *block, unsigned int n);
+void block_clean(CLGLOBAL block_t *block);
+CLGLOBAL block_t* block_add_drift(CLGLOBAL block_t *block, uint64_t *offsets, uint64_t nel, double length);
+CLGLOBAL block_t* block_add_cavity(CLGLOBAL block_t *block, uint64_t *offsets, uint64_t nel, double volt, double freq, double lag);
+CLGLOBAL block_t* block_add_align(CLGLOBAL block_t *block, uint64_t *offsets, uint64_t nel, double tilt, double dx, double dy);
+CLGLOBAL block_t* block_add_multipole(CLGLOBAL block_t *block, uint64_t *offsets, uint64_t nel, double *knl, unsigned int knl_len, double *ksl, unsigned int ksl_len, double length, double hxl, double hyl);
+CLGLOBAL block_t* block_add_block(CLGLOBAL block_t *block, uint64_t *offsets, uint64_t nel);
 
 #endif
