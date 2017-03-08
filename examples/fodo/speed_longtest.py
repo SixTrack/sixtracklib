4b3fad58396330ea2def84241cff4cedefc884e9
Riccardo De Maria <riccardo.de.maria@cern.ch>
2017-03-08 11:57:02 +0100

    update tests

--- examples/fodo/speed_longtest.py
+++ examples/fodo/speed_longtest.py
@@ -1,35 +1,41 @@
 #!/usr/bin/env python
+import os
+
 import numpy as np
 
-import sixtracktools
+from pyoptics import madlang, MadBeam
 import sixtracklib
 
+def newer(f1,f2):
+    if os.path.exists(f2):
+        return os.path.getmtime(f1)>os.path.getmtime(f2)
+    else:
+        return True
 
-six =sixtracktools.SixTrackInput('.')
-line,rest,iconv=six.expand_struct()
-names,types,args=zip(*line)
-idx=dict( (nn,ii) for ii,nn in enumerate(six.struct) if not 'BLOC' in nn)
-names2=np.array(names)[iconv]
+mad=madlang.open('bench.madx')
+line,rest=mad.bench.expand_struct()
+if newer('bench.madx','track.obs0001.p0001'):
+  os.system('madx bench.madx')
 
-
-sixtrackbeam=sixtracktools.SixDump3('dump3.dat')
+madbeam=MadBeam.open('track.obs0001.p0001','twiss_bench.tfs')
 
 block=sixtracklib.cBlock.from_line(line)
-bref=sixtracklib.cBeam.from_full_beam(sixtrackbeam.get_full_beam())
-bref=bref.reshape(-1,2)
+bref=sixtracklib.cBeam.from_full_beam(madbeam.get_full_beam())
+cbeam=bref.copy()[[0]]
 
 import time
 import sys
 import pyopencl
 
 def mkbench(npart,nturn):
+  nturn=int(nturn)
   block=sixtracklib.cBlock.from_line(line)
   cbeam=bref.copy().reshape(-1)[:npart]
   st=time.time()
   block.track_cl(cbeam,nturn=nturn,turnbyturn=True)
   st=time.time()-st
-  perfgpu=st/npart/nturn*1e6
-  print("GPU part %4d, turn %4d: %10.3f usec/part*turn"%(npart,nturn,perfgpu))
+  perfgpu=st/npart/nturn*1e9
+  print("GPU part %4d, turn %4d: %10.3f nsec/part*turn"%(npart,nturn,perfgpu))
 
   block=sixtracklib.cBlock.from_line(line)
   npart2=npart/100
@@ -37,8 +43,8 @@ def mkbench(npart,nturn):
   st=time.time()
   block.track(cbeam,nturn=nturn,turnbyturn=True)
   st=time.time()-st
-  perfcpu=st/npart2/nturn*1e6
-  print("CPU part %4d, turn %4d: %10.3f usec/part*turn"%(npart2,nturn,perfcpu))
+  perfcpu=st/npart2/nturn*1e9
+  print("CPU part %4d, turn %4d: %10.3f nsec/part*turn"%(npart2,nturn,perfcpu))
 
   print("GPU/CPU : %g"%(perfcpu/perfgpu))
   return st,npart,nturn,perfgpu,perfcpu
@@ -46,8 +52,7 @@ def mkbench(npart,nturn):
 out=open(time.strftime("bench_%Y%M%dT%H%m%S.txt"),'w')
 out.write("#%s"%pyopencl.get_platforms()[0].get_devices()[0])
 for npart in [100,1000,2000,5000,10000,20000]:
-    for nturn in [1,2,5,10]:
+    for nturn in [1e5,2e5,10e5]:
         st,npart,nturn,perfgpu,perfcpu=mkbench(npart,nturn)
         fmt="%5d %5d %10.3f %10.3f %10.3f\n"
         out.write(fmt%(npart,nturn,perfgpu,perfcpu,perfcpu/perfgpu))
-
