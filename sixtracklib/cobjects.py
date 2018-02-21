2c90cc8ab03de88438302073ef8b72b178170e15
Riccardo De Maria <riccardodemaria@gmail.com>
2018-02-21 19:08:06 +0100

    first implementation of nested objects

--- sixtracklib/cobjects.py
+++ sixtracklib/cobjects.py
@@ -1,4 +1,20 @@
+"""
+
+object have 64bit slots
+
+a property can be a scalar, a composity object or an array of scalars or composite objects
+
+length can be static or depends on constant properties that needs to be declared as such
+
+property needs to be indexed by increasiing offsets (TODO check they are consistent or sort by order)
+
+"""
+
+
 import numpy as np
+
+
+
 
 class CProp(object):
     def __init__(self,valuetype=None,offset=None,
@@ -10,20 +26,27 @@ class CProp(object):
         self.default=default
         self.const=const
     def resolve_length(self,const):
-        if isinstance(self.valuetype,CObject):
-            size=self.valuetype.get_length(const)
-        else:
-            size=1
         if isinstance(self.length,str):
             length=eval(self.length,{},const)
         else:
             length=self.length
-        if length is not None:
-            length*=size
         return length
+    def resolve_size(self,const):
+        if type(self.valuetype) is type and issubclass(self.valuetype,CObject):
+            size=self.valuetype._get_size(**const)
+        else:
+            size=1
+        length=self.resolve_length(const)
+        if length is None:
+            totsize=size
+        else:
+            totsize=length*size+1
+        return totsize
     def __get__(self,obj,type=None):
         name,offset,length=obj._attr[self.offset]
         shape=obj._shape.get(name)
+        if self.offset in obj._obj:
+            return obj._obj[self.offset]
         if length is None:
            val=obj._cbuffer.data[self.valuetype][offset]
         else:
@@ -88,10 +111,28 @@ class CBuffer(object):
 
 class CObject(object):
     @classmethod
-    def get_length(cls,const):
-        return cls(cbuffer = None,**const)._size
+    def _get_props(cls):
+        props=[(prop.offset,name,prop) for name,prop
+                                       in cls.__dict__.items()
+                                       if isinstance(prop,CProp)        ]
+        props.sort()
+        return props
+    @classmethod
+    def _get_size(cls,**nvargs):
+        props=cls._get_props()
+        const={}
+        for offset,name,prop in props:
+            if prop.const:
+                const[name]=nvargs[name]
+        size=0
+        for offset,name,prop in props:
+            size+=prop.resolve_size(const)
+        return size
+    @property
+    def _data(self):
+        return self._cbuffer.data[self._offset,self._offset+self._size]
     def __init__(self, cbuffer = None, **nvargs):
-        self._size = self._get_size(nvargs)
+        self._size = self._get_size(**nvargs)
         if cbuffer is None:
             self._cbuffer=CBuffer(self._size)
         else:
@@ -99,38 +140,17 @@ class CObject(object):
         self._offset=self._cbuffer.new_object()
         self._cbuffer.reserve_memory(self._size)
         self._cbuffer.next+=self._size
-        self._fill_args(nvargs)
         self._shape={}
-    def _get_props(self):
-        props=[(prop.offset,name,prop) for name,prop
-                                       in self.__class__.__dict__.items()
-                                       if isinstance(prop,CProp)        ]
-        props.sort()
-        return props
-    def _get_size(self,nvargs):
-        props=self._get_props()
-        const={}
-        for offset,name,prop in props:
-            if prop.const:
-                const[name]=nvargs[name]
-        size=0
-        self._lengths={}
-        self._names=[]
-        for offset,name,prop in props:
-            length=prop.resolve_length(const)
-            if length is None:
-                size+=1
-            else:
-                size+=length+1
-            self._lengths[name]=length
-            self._names.append(name)
-        return size
+        self._obj={}
+        self._fill_args(nvargs)
     def _fill_args(self,nvargs):
+        self._names=[]
         props=self._get_props()
         self._attr=[]
         lastarray=props[-1][0]+1
         for offset,name,prop in props:
-            length=self._lengths[name]
+            self._names.append(name)
+            length=prop.resolve_length(nvargs)
             if isinstance(prop.valuetype,str):
               if length is None:
                  attr_offset=self._offset+offset
@@ -144,8 +164,24 @@ class CObject(object):
                  lastarray+=length
                  self._cbuffer.data[prop.valuetype][attr_offset:attr_offset+length]= \
                                    nvargs.get(name,prop.default)
-            elif isinstance(prop.valuetype,CObject):
-                raise NotImplemented('Nested object not implemented')
+            elif type(prop.valuetype) is type and issubclass(prop.valuetype,CObject):
+                size=prop.valuetype._get_size(**nvargs)
+                if length is None:
+                    val=nvargs.get(name,{})
+                    attr_offset=self._offset+offset
+                    self._obj[offset]=prop.valuetype(cbuffer=self._cbuffer,**val)
+                    self._attr.append((name,attr_offset,None))
+                else:
+                    raise NotImplemented
+                    val=nvargs.get(name,[{}])
+                    attr_offset=self._offset+lastarray
+                    self._cbuffer.data['u64'][self._offset+offset]=lastarray
+                    self._attr.append((name,attr_offset,length))
+                    lastarray+=size*length
+                    for ival in val:
+                       self._cbuffer.data[attr_offset:attr_offset+size]=ival
+                       attr_offset=self._offset+lastarray
+
     def pretty_print(self):
         props=self._get_props()
         out=['%s('%(self.__class__.__name__)]
