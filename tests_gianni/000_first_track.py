23183ab95998ca830335cb757b290ad829312ea3
giadarol <giovanni.iadarola@cern.ch>
2018-02-23 23:28:16 +0100

    I would like to have longitudinal motion...
    but this is not possible in the absence of bending magnets

--- tests_gianni/000_first_track.py
+++ tests_gianni/000_first_track.py
@@ -4,21 +4,58 @@ sys.path.append('../../pyoptics/')
 import sixtracklib
 import numpy as np
 
+from scipy.constants import c as c_light
+pmass_eV = 938.272046e6
+
+V_RF = 100e6
+lag_RF = 0.5
+h_RF = 9000
+
+
+
 import tfsdata
 
 twdict = tfsdata.open('twiss.out')
 
 machine = sixtracklib.CBlock()
 
+length = twdict['param']['length'] 
+f_RF = h_RF*c_light/(length)
+
+#machine.add_Cavity(voltage=V_RF,frequency=f_RF,lag=lag_RF)
+
 for i_ele, name in enumerate(twdict['name']):
 	if twdict['keyword'][i_ele]=='MULTIPOLE':
-		machine.add_Multipole(name=name, knl=[0.0,twdict['k1l'][i_ele]])
+		machine.add_Multipole(name=name, knl=[twdict['k0l'][i_ele],twdict['k1l'][i_ele]], 
+								angle = twdict['k0l'][i_ele])
 	elif twdict['keyword'][i_ele]=='DRIFT':
 		machine.add_Drift(name=name, length=twdict['l'][i_ele])
 	else:
 		print('Skipped: %s'%name)
 
-bunch=sixtracklib.CParticles(npart=2)
+
+
+
+p0c_eV = 6500e9
+gamma0 = np.sqrt(p0c_eV**2+pmass_eV**2)/pmass_eV
+beta0 = p0c_eV/np.sqrt(p0c_eV**2+pmass_eV**2)
+
+delta = 0.#1e-3
+rpp = 1./(delta+1)
+pc_eV = p0c_eV/rpp
+gamma = np.sqrt(1. + (pc_eV/pmass_eV)**2)
+beta = np.sqrt(1.-1./gamma**2)
+rvv=beta/beta0
+psigma = pmass_eV*(gamma-gamma0)/(beta0*p0c_eV)
+
+bunch=sixtracklib.CParticles(npart=1, 
+		p0c=p0c_eV,
+		beta0 = beta0,
+		gamma0 = gamma0,
+		delta = delta,
+		rvv = rvv,
+		rpp = rpp,
+		psigma = psigma)
 bunch.x[0]=0.3
 bunch.y[0]=0.2
 
@@ -43,5 +80,10 @@ axf1 = pl.subplot(2,1,1)
 pl.plot(freq, np.abs(spec_x))
 axf2 = pl.subplot(2,1,2, sharex=axf1)
 pl.plot(freq, np.abs(spec_y))
+
+pl.figure(3)
+axl1 = pl.subplot(2,1,1)
+pl.plot(tbt.delta[:,0])
+
 
 pl.show()
