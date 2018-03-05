90e091bf3fb8c2bb481e2f9202bccf1d7ac58741
giadarol <giovanni.iadarola@cern.ch>
2018-03-05 12:16:32 +0100

    Struggling with closed orbit

--- tests_gianni/003_find_CO.py
+++ tests_gianni/003_find_CO.py
@@ -51,12 +51,17 @@ for i_ele in indices:
 		print('Skipped: %s'%name)
 
 #Add dipole corrector to distort the orbit
-machine.add_Multipole(name=name, knl=[20.e-6])
-machine.add_Multipole(name=name, ksl=[30.e-6])
+machine.add_Multipole(name=name, knl=[100.e-6])
+#machine.add_Multipole(name=name, ksl=[30.e-6])
 
-def one_turn_map(coord):
+# devide_by = np.array([1e-3, 1e-6, 1e-3, 1e-6, 1e-3, 1e-4])
+devide_by = np.array([1., 1., 1., 1., 1., 1.])
+
+def one_turn_map(coord_in):
 
 	# coord = np.array([x, px, y, py, sigma, delta])
+
+	coord = coord_in*devide_by
 
 	npart = 1
 
@@ -84,15 +89,22 @@ def one_turn_map(coord):
 
 	particles,ebe,tbt = machine.track_cl(bunch,nturns=1,elembyelem=None,turnbyturn=True)
 
-	return np.array([tbt.x[1], tbt.px[1], tbt.y[1], tbt.py[1], 
-					tbt.sigma[1], tbt.delta[1]])
+	coord =  np.array([tbt.x[1][0], tbt.px[1][0], tbt.y[1][0], tbt.py[1][0], 
+					tbt.sigma[1][0], tbt.delta[1][0]])
+
+	for ii in range(2, 6):
+		coord[ii] = 0.
+
+	return coord/devide_by
 
 
 # fxdpt = so.fixed_point(one_turn_map, np.array([0.,0.,0.,0.,0.,0.]))
 
 tominimize = lambda coord: np.sum((one_turn_map(coord)-coord)**2)
 
-res = so.minimize(tominimize, np.array([0.,0.,0.,0.,0.,0.]), tol=1e-10)
+
+
+res = so.minimize(tominimize, np.array([0.,0.,0.,0.,0.,0.]), tol=1e-20)
 
 npart = 1
 
@@ -112,11 +124,34 @@ bunch=sixtracklib.CParticles(npart=npart,
 		rvv = rvv,
 		rpp = rpp,
 		psigma = psigma)
-bunch.x+=0.001
-bunch.y+=0.002
+bunch.x+=0.00
+bunch.y+=0.00
 
-particles,ebe,tbt=machine.track_cl(bunch,nturns=512,elembyelem=True,turnbyturn=True)
+n_turns = 2048
 
+particles,ebe,tbt=machine.track_cl(bunch,nturns=n_turns,elembyelem=True,turnbyturn=True)
+
+# coord = np.array(6*[0.])
+# coord_list = []
+# for i_turn in xrange(n_turns):
+# 	coord_list.append(coord)
+# 	coord = one_turn_map(coord)
+
+# coord_mat = np.array(coord_list)
+# x_mean = np.mean(coord_mat[:,0])
+# px_mean = np.mean(coord_mat[:,1])
+# y_mean = np.mean(coord_mat[:,2])
+# py_mean = np.mean(coord_mat[:,3])
+
+x_mean = np.mean(tbt.x)
+px_mean = np.mean(tbt.px)
+y_mean = np.mean(tbt.y)
+py_mean = np.mean(tbt.py)
+sigma_mean = np.mean(tbt.sigma)
+delta_mean = np.mean(tbt.delta)
+
+# res2 = so.minimize(tominimize, np.array([x_mean,px_mean,y_mean, py_mean,sigma_mean,delta_mean]), tol=1e-20)
+# res3 = so.minimize(tominimize, res.x, tol=1e-20)
 
 import matplotlib.pyplot as pl
 pl.close('all')
@@ -134,6 +169,8 @@ spd = pl.subplot(2,2,3, sharex=spx)
 spphase = pl.subplot2grid(shape=(2,2), loc=(0,1), rowspan=2)
 
 
+
+
 import numpy.fft as fft
 
 for i_part in range(npart):
@@ -143,6 +180,7 @@ for i_part in range(npart):
 	spec_y = fft.fft(tbt.y[:,i_part])
 
 	spx.plot(tbt.x[:,i_part])
+	# spx.plot(coord_mat[:,0])
 	spy.plot(tbt.y[:,i_part])
 
 	spfx.plot(freq, np.abs(spec_x))
@@ -166,4 +204,33 @@ spebe3 = pl.subplot(3,1,3, sharex=spebe1)
 spebe1.plot(np.mean(ebe.x[:,:,0], axis=0))
 spebe2.plot(np.mean(ebe.y[:,:,0], axis=0))
 spebe3.plot(np.mean(ebe.sigma[:,:,0], axis=0))
+
+pl.figure(4)
+spebep1 = pl.subplot(3,1,1)
+spebep2 = pl.subplot(3,1,2, sharex=spebe1, sharey=spebe1)
+spebep3 = pl.subplot(3,1,3, sharex=spebe1)
+spebep1.plot(np.mean(ebe.px[:,:,0], axis=0))
+spebep2.plot(np.mean(ebe.py[:,:,0], axis=0))
+spebep3.plot(np.mean(ebe.delta[:,:,0], axis=0))
+
+x_vect = np.linspace(-3e-2, 3e-2, 110)
+pl.figure(1000)
+pl.plot(x_vect, map(lambda x: tominimize(np.array([x, px_mean, 0, 0., 0., 0.])), x_vect))
+pl.axvline(x = x_mean)
+
+px_vect = np.linspace(-3e-4, 3e-4, 100)
+pl.figure(2000)
+pl.plot(px_vect, map(lambda px: tominimize(np.array([x_mean, px, 0, 0., 0., 0.])), px_vect))
+pl.axvline(x = px_mean)
+
+mat = np.zeros((len(x_vect), len(px_vect)))
+
+# for ix, x in enumerate(x_vect):
+# 	print ix
+# 	for ipx, px in enumerate(px_vect):
+# 		mat[ix, ipx] = tominimize(np.array([x, px, 0, 0., 0., 0.]))
+
+# pl.figure(100)
+# pl.pcolormesh(x_vect, px_vect, np.log10(mat).T)
+
 pl.show()
