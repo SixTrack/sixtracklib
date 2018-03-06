9e5e283efd63d41a5370818cee03aaeb6262ea34
giadarol <giovanni.iadarola@cern.ch>
2018-03-06 12:06:22 +0100

    Added example in which it segfaults (to be insvestigated)

---
+++ tests_gianni/004_foundsegfault.py
@@ -0,0 +1,17 @@
+import sys; sys.path.append('../')
+
+import sixtracklib
+machine = sixtracklib.CBlock()
+machine.add_Drift(length=1.)
+
+print('This works')
+bunch=sixtracklib.CParticles(npart=1)
+particles,ebe,tbt=machine.track_cl(bunch,nturns=1,elembyelem=True,turnbyturn=True)
+bunch=sixtracklib.CParticles(npart=1)
+particles,ebe,tbt=machine.track_cl(bunch,nturns=1,elembyelem=True,turnbyturn=True)
+print('Done')
+
+print('This gives segfault:')
+bunch=sixtracklib.CParticles(npart=1)
+particles,ebe,tbt=machine.track_cl(bunch,nturns=1,elembyelem=True,turnbyturn=True)
+particles,ebe,tbt=machine.track_cl(bunch,nturns=1,elembyelem=True,turnbyturn=True)
