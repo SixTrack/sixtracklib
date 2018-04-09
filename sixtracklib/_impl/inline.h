d8ba96e012a2686d42f6da60e9d921c3cab2bf8b
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-04-09 19:14:41 +0200

    added helper header to encapsulate the inline keyword

---
+++ sixtracklib/_impl/inline.h
@@ -0,0 +1,12 @@
+#ifndef SIXTRACKLIB_SIXTRACKLIB__IMPL_INLINE_H__
+#define SIXTRACKLIB_SIXTRACKLIB__IMPL_INLINE_H__
+
+#if !defined( _GPUCODE )    
+    #if !defined( SIXTRL_INLINE )
+        #define SIXTRL_INLINE inline 
+    #endif /* !defined( SIXTRL_INLINE ) */    
+#endif /* !defined( _GPUCODE ) */
+
+#endif /* SIXTRACKLIB_SIXTRACKLIB__IMPL_INLINE_H__ */
+
+/* end: sixtracklib/_impl/inline.h */
