964583e898b153b3d8762827097de70cd2f66292
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-04-11 09:50:13 +0200

    Change tracking code implementation to use new Particles API
    - Use _get_*_value and _set_*_value accessor functions to manipulate
      the contents of the Particles

--- sixtracklib/common/impl/track_impl.h
+++ sixtracklib/common/impl/track_impl.h
@@ -12,26 +12,26 @@
         \
     T const ONE    = ( T )1; \
     T const TWO    = ( T )2; \
-    T const rpp    = NS(Particles_get_rpp)(   ( particles ), ( ip ) ); \
-    T const rvv    = NS(Particles_get_rvv)(   ( particles ), ( ip ) ); \
-    T const px     = NS(Particles_get_px)(    ( particles ), ( ip ) ) * rpp; \
-    T const py     = NS(Particles_get_py)(    ( particles ), ( ip ) ) * rpp; \
+    T const rpp    = NS(Particles_get_rpp_value)(   ( particles ), ( ip ) ); \
+    T const rvv    = NS(Particles_get_rvv_value)(   ( particles ), ( ip ) ); \
+    T const px     = NS(Particles_get_px_value)(    ( particles ), ( ip ) ) * rpp; \
+    T const py     = NS(Particles_get_py_value)(    ( particles ), ( ip ) ) * rpp; \
     T const dsigma = ( ONE - rvv * ( ONE + ( px * px + py * py ) / TWO ) );  \
         \
-    T x            = NS(Particles_get_x)(     ( particles ), ( ip ) ); \
-    T y            = NS(Particles_get_y)(     ( particles ), ( ip ) ); \
-    T s            = NS(Particles_get_s)(     ( particles ), ( ip ) ); \
-    T sigma        = NS(Particles_get_sigma)( ( particles ), ( ip ) ); \
+    T x            = NS(Particles_get_x_value)(     ( particles ), ( ip ) ); \
+    T y            = NS(Particles_get_y_value)(     ( particles ), ( ip ) ); \
+    T s            = NS(Particles_get_s_value)(     ( particles ), ( ip ) ); \
+    T sigma        = NS(Particles_get_sigma_value)( ( particles ), ( ip ) ); \
         \
     x             += ( length ) * px; \
     y             += ( length ) * py; \
     sigma         += ( length ) * dsigma; \
     s             += ( length ); \
         \
-    NS(Particles_set_sigma)( ( particles ), ( ip ), sigma ); \
-    NS(Particles_set_x)(     ( particles ), ( ip ), x     ); \
-    NS(Particles_set_y)(     ( particles ), ( ip ), y     ); \
-    NS(Particles_set_s)(     ( particles ), ( ip ), s     ); \
+    NS(Particles_set_sigma_value)( ( particles ), ( ip ), sigma ); \
+    NS(Particles_set_x_value)(     ( particles ), ( ip ), x     ); \
+    NS(Particles_set_y_value)(     ( particles ), ( ip ), y     ); \
+    NS(Particles_set_s_value)(     ( particles ), ( ip ), s     ); \
         \
     /* ------------------------------------------------------------------------- */ \
     /* -----   End Of SIXTRACKLIB_DRIFT_TRACK_IMPL                               */ \
@@ -49,29 +49,29 @@
     /* ------------------------------------------------------------------------- */ \
         \
     T const ONE   = ( T )1; \
-    T const delta = NS(Particles_get_delta)( ( particles ), ( ip ) ); \
-    T const beta0 = NS(Particles_get_beta0)( ( particles ), ( ip ) ); \
-    T sigma       = NS(Particles_get_sigma)( ( particles ), ( ip ) ); \
-    T const px    = NS(Particles_get_px)(    ( particles ), ( ip ) ); \
-    T const py    = NS(Particles_get_py)(    ( particles ), ( ip ) ); \
+    T const delta = NS(Particles_get_delta_value)( ( particles ), ( ip ) ); \
+    T const beta0 = NS(Particles_get_beta0_value)( ( particles ), ( ip ) ); \
+    T sigma       = NS(Particles_get_sigma_value)( ( particles ), ( ip ) ); \
+    T const px    = NS(Particles_get_px_value)(    ( particles ), ( ip ) ); \
+    T const py    = NS(Particles_get_py_value)(    ( particles ), ( ip ) ); \
      \
     T const opd   = delta + ONE; \
     T const lpzi  = ( length ) / sqrt( opd * opd - px * px - py * py ); \
     T const lbzi  = ( beta0 * beta0 * sigma + ONE ) * lpzi; \
         \
-    T x           = NS(Particles_get_x)( ( particles ), ( ip ) ); \
-    T y           = NS(Particles_get_y)( ( particles ), ( ip ) ); \
-    T s           = NS(Particles_get_s)( ( particles ), ( ip ) ); \
+    T x           = NS(Particles_get_x_value)( ( particles ), ( ip ) ); \
+    T y           = NS(Particles_get_y_value)( ( particles ), ( ip ) ); \
+    T s           = NS(Particles_get_s_value)( ( particles ), ( ip ) ); \
         \
     x            += px * lpzi; \
     y            += py * lpzi; \
     s            += ( length ); \
     sigma        += ( length ) - lbzi; \
         \
-    NS(Particles_set_x)(     ( particles ), ( ip ), x     ); \
-    NS(Particles_set_y)(     ( particles ), ( ip ), y     ); \
-    NS(Particles_set_s)(     ( particles ), ( ip ), s     ); \
-    NS(Particles_set_sigma)( ( particles ), ( ip ), sigma ); \
+    NS(Particles_set_x_value)(     ( particles ), ( ip ), x     ); \
+    NS(Particles_set_y_value)(     ( particles ), ( ip ), y     ); \
+    NS(Particles_set_s_value)(     ( particles ), ( ip ), s     ); \
+    NS(Particles_set_sigma_value)( ( particles ), ( ip ), sigma ); \
         \
     /* ------------------------------------------------------------------------- */ \
     /* -----   End Of SIXTRACKLIB_DRIFT_EXACT_TRACK_IMPL                         */ \
