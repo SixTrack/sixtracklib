909ccb53ddfa4c240ddd76536652e06750e29733
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-04-12 16:28:43 +0200

    starting to assemble the main/convenience include header

--- sixtracklib/sixtracklib.h
+++ sixtracklib/sixtracklib.h
@@ -1,25 +1,26 @@
 #ifndef SIXTRACKLIB_SIXTRACKLIB_H__
 #define SIXTRACKLIB_SIXTRACKLIB_H__
 
-#if !defined( NAMESPACE )
-    #define NAMESPACE st_
-    #define SIXTRACKLIB_SIXTRACKLIB_UNDEF_NAMESPACE 1
-#endif /* !defined( NAMESPACE ) */
+#if !defined( __NAMESPACE )
+    #define __NAMESPACE st_
+    #define __SIXTRACKLIB_SIXTRACKLIB_UNDEF_NAMESPACE 1
+#endif /* !defined( __NAMESPACE ) */
 
 #include "sixtracklib/_impl/namespace_begin.h"
 
 /* ------------------------------------------------------------------------- */
 
-
-
-
+#include "sixtracklib/common/mem_pool.h"
+#include "sixtracklib/common/particles.h"
+#include "sixtracklib/common/impl/particles_type.h"
 
 /* ------------------------------------------------------------------------- */
 
 #include "sixtracklib/_impl/namespace_end.h"
 
 #if defined( SIXTRACKLIB_SIXTRACKLIB_UNDEF_NAMESPACE )
-    #undef NAMESPACE 
+    #undef __NAMESPACE 
+    #undef __SIXTRACKLIB_SIXTRACKLIB_UNDEF_NAMESPACE
 #endif /* defined( SIXTRACKLIB_SIXTRACKLIB_UNDEF_NAMESPACE ) */
 
 #endif /* SIXTRACKLIB_SIXTRACKLIB_H__ */
