bb85f99a8feee9a937c1e87ae801b22f3c311014
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-08-22 13:33:02 +0200

    tests/sixtracklib/common: Update NS(Buffer) related unit-test collection
    - In the CommonBufferTests::ReconstructFromCObjectFile unit-test, generate the binary file
      on the fly and read it back. This ensures, that the file should always be recent and
      available
    
    - Update API to reflect recent changes in NS(Buffer)

--- tests/sixtracklib/common/test_buffer.cpp
+++ tests/sixtracklib/common/test_buffer.cpp
@@ -29,6 +29,8 @@ namespace sixtrl
     };
 }
 
+/* ************************************************************************* */
+
 TEST( CommonBufferTests, InitOnExistingFlatMemory)
 {
     std::vector< unsigned char > too_small( 36u, uint8_t{ 0 } );
@@ -50,7 +52,9 @@ TEST( CommonBufferTests, InitOnExistingFlatMemory)
     ASSERT_TRUE( success == 0 );
 }
 
-TEST( CommonBufferTests, InitFlatMemoryDataStoreAddObjects )
+/* ************************************************************************* */
+
+TEST( CommonBufferTests, InitFlatMemoryDataStoreAddObjectsRemapAndCompare )
 {
     using my_obj_t = sixtrl::MyObj;
     using obj_t    = NS(Object);
@@ -549,5 +553,235 @@ TEST( CommonBufferTests, InitFlatMemoryDataStoreAddObjects )
     st_Buffer_free( &buffer );
     st_Buffer_free( &cmp_buffer );
 }
+
+/* ************************************************************************* */
+
+TEST( CommonBufferTests, ReconstructFromCObjectFile )
+{
+    using my_obj_t = sixtrl::MyObj;
+    using obj_t    = NS(Object);
+
+    /* --------------------------------------------------------------------- */
+    /* PREPARE DATA OBJECTS                                                  */
+    /* --------------------------------------------------------------------- */
+
+    std::vector< unsigned char > data_buffer( 4096u, uint8_t{ 0 } );
+
+    st_buffer_size_t const N = 3u;
+    constexpr st_buffer_size_t NUM_DATAPTRS = 2u;
+
+    st_buffer_size_t const offsets[ NUM_DATAPTRS ] =
+    {
+        offsetof( my_obj_t, d ),
+        offsetof( my_obj_t, e )
+    };
+
+    st_buffer_size_t const type_sizes[ NUM_DATAPTRS ] =
+    {
+        sizeof( uint8_t ), sizeof( double )
+    };
+
+    st_buffer_size_t attr_counts[ NUM_DATAPTRS ] =
+    {
+        st_buffer_size_t{ 0 },
+        st_buffer_size_t{ 0 }
+    };
+
+    constexpr st_buffer_size_t num_d_values = 4;
+    constexpr st_buffer_size_t num_e_values = 2;
+
+    uint8_t obj1_d_values[ num_d_values ] = { 31, 32, 33, 34 };
+    double  obj1_e_values[ num_e_values ] = { 35.0, 36.0 };
+
+    attr_counts[ 0 ] = num_d_values;
+    attr_counts[ 1 ] = num_e_values;
+
+    my_obj_t cmp_my_obj[ 3 ];
+
+    my_obj_t& obj1 = cmp_my_obj[ 0 ];
+    obj1.type_id = 3u;
+    obj1.a       = 25;
+    obj1.b       = 26.0;
+    obj1.c[ 0 ]  = 27.0;
+    obj1.c[ 1 ]  = 28.0;
+    obj1.c[ 2 ]  = 29.0;
+    obj1.c[ 3 ]  = 30.0;
+    obj1.d       = &obj1_d_values[ 0 ];
+    obj1.e       = &obj1_e_values[ 0 ];
+
+    /* --------------------------------------------------------------------- */
+
+    uint8_t obj2_d_values[ num_d_values ] =
+    {
+        uint8_t{ 106 }, uint8_t{ 107 }, uint8_t{ 108 }, uint8_t{ 109 }
+    };
+
+    double obj2_e_values[ num_e_values ] = { 110.0, 111.0 };
+
+    my_obj_t& obj2 = cmp_my_obj[ 1 ];
+    obj2.type_id = 4u;
+    obj2.a       = 100;
+    obj2.b       = 101.0;
+    obj2.c[ 0 ]  = 102.0;
+    obj2.c[ 1 ]  = 103.0;
+    obj2.c[ 2 ]  = 104.0;
+    obj2.c[ 3 ]  = 105.0;
+    obj2.d       = &obj2_d_values[ 0 ];
+    obj2.e       = &obj2_e_values[ 0 ];
+
+    /* --------------------------------------------------------------------- */
+
+    my_obj_t& obj3 = cmp_my_obj[ 2 ];
+    obj3.type_id = 5u;
+    obj3.a       = 200;
+    obj3.b       = 201.0;
+    obj3.c[ 0 ]  = 202.0;
+    obj3.c[ 1 ]  = 203.0;
+    obj3.c[ 2 ]  = 204.0;
+    obj3.c[ 3 ]  = 205.0;
+    obj3.d       = nullptr;
+    obj3.e       = nullptr;
+
+    /* --------------------------------------------------------------------- */
+    /* WRITE BUFFER TO BINARY FILE */
+    /* --------------------------------------------------------------------- */
+
+    char const PATH_TO_BINARY_FILE[] = "./test.np";
+    FILE* fp = std::fopen( PATH_TO_BINARY_FILE, "rb" );
+
+    if( fp == nullptr )
+    {
+        fp = std::fopen( PATH_TO_BINARY_FILE, "wb" );
+        ASSERT_TRUE( fp != nullptr );
+
+        st_Buffer buffer;
+        st_Buffer_preset( &buffer );
+
+        int success = st_Buffer_init_on_flat_memory_detailed( &buffer,
+                data_buffer.data(), data_buffer.size(), N, 48u, 8u, 1u );
+
+        ASSERT_TRUE( success == 0 );
+
+        obj_t* ptr_object = st_Buffer_add_object( &buffer, &obj1, sizeof( obj1 ),
+            obj1.type_id, NUM_DATAPTRS, &offsets[ 0 ],
+                &type_sizes[ 0 ], &attr_counts[ 0 ] );
+
+        ASSERT_TRUE( ptr_object != nullptr );
+
+        ptr_object = st_Buffer_add_object( &buffer, &obj2, sizeof( obj2 ),
+            obj2.type_id, NUM_DATAPTRS, &offsets[ 0 ],
+                &type_sizes[ 0 ], &attr_counts[ 0 ] );
+
+        ASSERT_TRUE( ptr_object != nullptr );
+
+        st_buffer_size_t const cnt = std::fwrite( ( unsigned char const* )(
+            uintptr_t )st_Buffer_get_data_begin_addr( &buffer ),
+            st_Buffer_get_size( &buffer ), st_buffer_size_t{ 1 }, fp );
+
+        ASSERT_TRUE( cnt == st_buffer_size_t{ 1 } );
+
+        std::fflush( fp );
+        std::fclose( fp );
+        fp = nullptr;
+    }
+    else
+    {
+        std::fclose( fp );
+        fp = nullptr;
+    }
+
+    ASSERT_TRUE( fp == nullptr );
+
+    fp = std::fopen( PATH_TO_BINARY_FILE, "rb" );
+    ASSERT_TRUE( fp != nullptr );
+
+    std::fseek( fp, 0, SEEK_END );
+    long int const fp_end_pos = std::ftell( fp );
+
+    std::fseek( fp, 0, SEEK_SET );
+    long int const fp_begin_pos = std::ftell( fp );
+
+    std::size_t const buffer_size = ( fp_end_pos >= fp_begin_pos )
+        ? static_cast< std::size_t >( fp_end_pos - fp_begin_pos )
+        : std::size_t{ 0 };
+
+    std::vector< unsigned char > base_buffer( buffer_size, uint8_t{ 0 } );
+    ASSERT_TRUE( buffer_size > std::size_t{ 0 } );
+
+    unsigned char* data_buffer_begin = base_buffer.data();
+    ASSERT_TRUE( data_buffer_begin != nullptr );
+
+    std::size_t cnt = std::fread( data_buffer_begin, buffer_size, 1u, fp );
+    ASSERT_TRUE( cnt == 1u );
+
+    if( fp != nullptr )
+    {
+        std::fclose( fp );
+        fp = nullptr;
+    }
+
+    ASSERT_TRUE( fp == nullptr );
+    std::remove( PATH_TO_BINARY_FILE );
+
+    st_Buffer buffer;
+    st_Buffer_preset( &buffer );
+
+    int success = st_Buffer_init_from_data( &buffer, data_buffer_begin );
+    ASSERT_TRUE( success == 0 );
+
+    ASSERT_TRUE( st_Buffer_get_size( &buffer ) == buffer_size );
+    ASSERT_TRUE( st_Buffer_get_num_of_objects( &buffer ) == 2u );
+
+    st_Object const* obj_it  = st_Buffer_get_const_objects_begin( &buffer );
+    st_Object const* obj_end = st_Buffer_get_const_objects_end( &buffer );
+
+    sixtrl::MyObj const* cmp_obj_it = &cmp_my_obj[ 0 ];
+
+    for( ; obj_it != obj_end ; ++obj_it, ++cmp_obj_it )
+    {
+        using ptr_my_obj_t = sixtrl::MyObj const*;
+
+        ASSERT_TRUE( st_Object_get_type_id( obj_it ) == cmp_obj_it->type_id );
+        ASSERT_TRUE( st_Object_get_size( obj_it ) > sizeof( sixtrl::MyObj ) );
+        ASSERT_TRUE( st_Object_get_const_begin_ptr( obj_it ) != nullptr );
+
+        ptr_my_obj_t ptr_my_obj = reinterpret_cast< ptr_my_obj_t >(
+                st_Object_get_const_begin_ptr( obj_it ) );
+
+        ASSERT_TRUE( ptr_my_obj != nullptr );
+        ASSERT_TRUE( ptr_my_obj->type_id == st_Object_get_type_id( obj_it ) );
+
+        ASSERT_TRUE( ptr_my_obj->a == cmp_obj_it->a );
+        ASSERT_TRUE( std::fabs( ( ptr_my_obj->b - cmp_obj_it->b ) <
+                        std::numeric_limits< double >::epsilon() ) );
+
+        for( std::size_t ii = 0u ; ii < 4u ; ++ii )
+        {
+            ASSERT_TRUE( std::fabs( ( ptr_my_obj->c[ ii ] -
+                cmp_obj_it->c[ ii ] ) < std::numeric_limits<
+                    double >::epsilon() ) );
+        }
+
+        ASSERT_TRUE( ptr_my_obj->d != nullptr );
+        ASSERT_TRUE( cmp_my_obj->d != nullptr );
+
+        for( std::size_t ii = 0u ; ii < 4u ; ++ii )
+        {
+            ASSERT_TRUE( std::fabs( ( ptr_my_obj->d[ ii ] -
+                cmp_obj_it->d[ ii ] ) < std::numeric_limits<
+                    double >::epsilon() ) );
+        }
+
+        ASSERT_TRUE( ptr_my_obj->e != nullptr );
+        ASSERT_TRUE( cmp_obj_it->e != nullptr );
+        ASSERT_TRUE( ptr_my_obj->e[ 0 ] == cmp_obj_it->e[ 0 ] );
+        ASSERT_TRUE( ptr_my_obj->e[ 1 ] == cmp_obj_it->e[ 1 ] );
+    }
+
+    st_Buffer_free( &buffer );
+
+}
+
+/* ************************************************************************* */
 
 /* end: tests/sixtracklib/common/test_buffer.cpp */
