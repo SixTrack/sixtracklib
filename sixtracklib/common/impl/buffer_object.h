0ef0e03fb216492875e714595cec9ea01fa1c760
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-08-28 13:23:31 +0200

    sixtracklib/common: move NS(Object) API to impl/buffer_object.h

--- sixtracklib/common/impl/buffer_object.h
+++ sixtracklib/common/impl/buffer_object.h
@@ -24,6 +24,29 @@ extern "C" {
 
 /* ========================================================================= */
 
+SIXTRL_FN SIXTRL_STATIC  NS(Object)* NS(Object_preset)(
+    struct NS(Object)* SIXTRL_RESTRICT object );
+
+SIXTRL_FN SIXTRL_STATIC NS(buffer_addr_t) NS(Object_get_begin_addr)(
+    const struct NS(Object) *const SIXTRL_RESTRICT object );
+
+SIXTRL_FN SIXTRL_STATIC void NS(Object_set_begin_addr)(
+    struct NS(Object)* SIXTRL_RESTRICT object,
+    NS(buffer_addr_t) const begin_addr );
+
+SIXTRL_FN SIXTRL_STATIC  NS(object_type_id_t) NS(Object_get_type_id)(
+    const struct NS(Object) *const SIXTRL_RESTRICT object );
+
+SIXTRL_FN SIXTRL_STATIC  void NS(Object_set_type_id)(
+    struct NS(Object)* SIXTRL_RESTRICT object,
+    NS(object_type_id_t) const type_id );
+
+SIXTRL_FN SIXTRL_STATIC  NS(buffer_size_t) NS(Object_get_size)(
+    const struct NS(Object) *const SIXTRL_RESTRICT object );
+
+SIXTRL_FN SIXTRL_STATIC  void NS(Object_set_size)(
+    struct NS(Object)* SIXTRL_RESTRICT object, NS(buffer_size_t) const size );
+
 SIXTRL_FN SIXTRL_STATIC SIXTRL_DATAPTR_DEC unsigned char const*
 NS(Object_get_const_begin_ptr)(
     SIXTRL_ARGPTR_DEC const struct NS(Object) *const SIXTRL_RESTRICT object );
@@ -67,6 +90,100 @@ extern "C" {
 #endif /* !defined( _GPUCODE ) && defined( __cplusplus ) */
 
 /* ========================================================================= */
+
+SIXTRL_INLINE NS(Object)* NS(Object_preset)(
+    NS(Object)* SIXTRL_RESTRICT object )
+{
+    if( object != SIXTRL_NULLPTR )
+    {
+        NS(Object_set_begin_addr)( object, 0u );
+        NS(Object_set_type_id)( object, 0u );
+        NS(Object_set_size)( object, 0u );
+    }
+
+    return object;
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+SIXTRL_INLINE NS(buffer_addr_t) NS(Object_get_begin_addr)(
+    const NS(Object) *const SIXTRL_RESTRICT object )
+{
+    #if !defined( NDEBUG )
+    typedef unsigned char const*    ptr_to_raw_t;
+    #endif /* !defined( NDEBUG ) */
+
+    typedef NS(buffer_addr_t)       address_t;
+
+    SIXTRL_ASSERT(
+        (   sizeof( ptr_to_raw_t ) >= sizeof( address_t ) ) ||
+        ( ( sizeof( ptr_to_raw_t ) == 4u ) &&
+          ( sizeof( address_t    ) == 8u ) &&
+          ( ( ( object != SIXTRL_NULLPTR ) &&
+              ( ( ( address_t )NS(BufferMem_get_limit_offset_max)() >
+                object->begin_addr ) ) ) ||
+            (   object == SIXTRL_NULLPTR ) ) ) );
+
+    return ( object != SIXTRL_NULLPTR ) ? object->begin_addr : ( address_t )0u;
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+SIXTRL_INLINE void NS(Object_set_begin_addr)(
+    NS(Object)* SIXTRL_RESTRICT object, NS(buffer_addr_t) const begin_addr )
+{
+    typedef unsigned char const*    ptr_to_raw_t;
+    typedef NS(buffer_addr_t)       address_t;
+
+    SIXTRL_ASSERT(
+        (   sizeof( ptr_to_raw_t ) >= sizeof( address_t ) ) ||
+        ( ( sizeof( ptr_to_raw_t ) == 4u ) &&
+          ( sizeof( address_t    ) == 8u ) &&
+          ( ( address_t )NS(BufferMem_get_limit_offset_max)() >
+              begin_addr ) ) );
+
+    if( object != SIXTRL_NULLPTR ) object->begin_addr = begin_addr;
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+SIXTRL_INLINE NS(object_type_id_t) NS(Object_get_type_id)(
+    const NS(Object) *const SIXTRL_RESTRICT object )
+{
+    return ( object != SIXTRL_NULLPTR )
+        ? object->type_id : ( NS(object_type_id_t ) )0;
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+SIXTRL_INLINE void NS(Object_set_type_id)(
+    NS(Object)* SIXTRL_RESTRICT object, NS(object_type_id_t) const type_id )
+{
+    if( object != SIXTRL_NULLPTR ) object->type_id = type_id;
+    return;
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+SIXTRL_INLINE NS(buffer_size_t) NS(Object_get_size)(
+    const NS(Object) *const SIXTRL_RESTRICT object )
+{
+    return ( object != SIXTRL_NULLPTR )
+        ? object->size : ( NS(buffer_size_t) )0u;
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+SIXTRL_INLINE void NS(Object_set_size)(
+    NS(Object)* SIXTRL_RESTRICT object, NS(buffer_size_t) const size )
+{
+    if( object != SIXTRL_NULLPTR )
+    {
+        object->size = size;
+    }
+
+    return;
+}
 
 SIXTRL_INLINE SIXTRL_ARGPTR_DEC SIXTRL_DATAPTR_DEC unsigned char const*
 NS(Object_get_const_begin_ptr)(
