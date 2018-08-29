411008534301d5531c592fbe33617d30e57c29c6
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-08-29 17:38:10 +0200

    tests/sixtracklib/common: Transition unit-test for NS(BufferMem_*) to NS(ManagedBuffer_*) API
    - Also: relabel the unit-tests according to the used API (i.e. prefix by C99 / CXX) to allow
      better diagnostics

--- tests/sixtracklib/common/test_managed_buffer_c99.cpp
+++ tests/sixtracklib/common/test_managed_buffer_c99.cpp
@@ -11,12 +11,12 @@
 #include "sixtracklib/testlib.h"
 
 #include "sixtracklib/_impl/definitions.h"
-#include "sixtracklib/common/impl/buffer_mem.h"
+#include "sixtracklib/common/impl/managed_buffer.h"
 
 /* ========================================================================= */
 /* ====  Test flat-memory based minimal buffer implementation                */
 
-TEST( C99_CommonBufferMem, InitAndReserve )
+TEST( C99_CommonManagedBuffer, InitAndReserve )
 {
     using size_t = ::st_buffer_size_t;
 
@@ -34,7 +34,7 @@ TEST( C99_CommonBufferMem, InitAndReserve )
     size_t max_num_dataptrs             = size_t{ 0 };
     size_t max_num_garbage_range        = size_t{ 0 };
 
-    size_t predicted_size = st_BufferMem_calculate_buffer_length( begin,
+    size_t predicted_size = st_ManagedBuffer_calculate_buffer_length( begin,
         max_num_objects, max_num_slots, max_num_dataptrs,
             max_num_garbage_range, slot_size );
 
@@ -42,38 +42,38 @@ TEST( C99_CommonBufferMem, InitAndReserve )
 
     size_t current_buffer_size  = size_t{ 0 };
 
-    int success = st_BufferMem_init( begin, &current_buffer_size,
+    int success = st_ManagedBuffer_init( begin, &current_buffer_size,
         max_num_objects, max_num_slots, max_num_dataptrs,
             max_num_garbage_range, data_buffer.size(), slot_size );
 
     ASSERT_TRUE( success == 0 );
     ASSERT_TRUE( predicted_size == current_buffer_size );
 
-    ASSERT_TRUE( st_BufferMem_get_section_max_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_max_num_entities(
         begin, SLOTS_ID, slot_size ) == max_num_slots );
 
-    ASSERT_TRUE( st_BufferMem_get_section_max_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_max_num_entities(
         begin, OBJECTS_ID, slot_size ) == max_num_objects );
 
-    ASSERT_TRUE( st_BufferMem_get_section_max_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_max_num_entities(
         begin, DATAPTRS_ID, slot_size ) == max_num_dataptrs );
 
-    ASSERT_TRUE( st_BufferMem_get_section_max_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_max_num_entities(
         begin, GARBAGE_ID, slot_size ) == max_num_garbage_range );
 
-    ASSERT_TRUE( st_BufferMem_get_section_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_num_entities(
         begin, SLOTS_ID, slot_size ) == size_t{ 0 } );
 
-    ASSERT_TRUE( st_BufferMem_get_section_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_num_entities(
         begin, OBJECTS_ID, slot_size ) == size_t{ 0 } );
 
-    ASSERT_TRUE( st_BufferMem_get_section_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_num_entities(
         begin, DATAPTRS_ID, slot_size ) == size_t{ 0 } );
 
-    ASSERT_TRUE( st_BufferMem_get_section_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_num_entities(
         begin, GARBAGE_ID, slot_size ) == size_t{ 0 } );
 
-    ASSERT_TRUE( st_BufferMem_get_buffer_length( begin, slot_size ) ==
+    ASSERT_TRUE( st_ManagedBuffer_get_buffer_length( begin, slot_size ) ==
         current_buffer_size );
 
     /* -------------------------------------------------------------------- */
@@ -83,45 +83,45 @@ TEST( C99_CommonBufferMem, InitAndReserve )
     max_num_dataptrs      = size_t{ 0 };
     max_num_garbage_range = size_t{ 0 };
 
-    predicted_size = st_BufferMem_calculate_buffer_length( begin,
+    predicted_size = st_ManagedBuffer_calculate_buffer_length( begin,
         max_num_objects, max_num_slots, max_num_dataptrs,
             max_num_garbage_range, slot_size );
 
     ASSERT_TRUE( predicted_size > current_buffer_size );
     ASSERT_TRUE( predicted_size < data_buffer.size()  );
 
-    ASSERT_TRUE( st_BufferMem_can_reserve( begin, &current_buffer_size,
+    ASSERT_TRUE( st_ManagedBuffer_can_reserve( begin, &current_buffer_size,
         max_num_objects, max_num_slots, max_num_dataptrs,
             max_num_garbage_range, data_buffer.size(), slot_size ) );
 
-    success = st_BufferMem_reserve( begin, &current_buffer_size,
+    success = st_ManagedBuffer_reserve( begin, &current_buffer_size,
         max_num_objects, max_num_slots, max_num_dataptrs,
             max_num_garbage_range, data_buffer.size(), slot_size );
 
     ASSERT_TRUE( success == 0 );
 
-    ASSERT_TRUE( st_BufferMem_get_section_max_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_max_num_entities(
         begin, SLOTS_ID, slot_size ) == max_num_slots );
 
-    ASSERT_TRUE( st_BufferMem_get_section_max_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_max_num_entities(
         begin, OBJECTS_ID, slot_size ) == max_num_objects );
 
-    ASSERT_TRUE( st_BufferMem_get_section_max_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_max_num_entities(
         begin, DATAPTRS_ID, slot_size ) == max_num_dataptrs );
 
-    ASSERT_TRUE( st_BufferMem_get_section_max_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_max_num_entities(
         begin, GARBAGE_ID, slot_size ) == max_num_garbage_range );
 
-    ASSERT_TRUE( st_BufferMem_get_section_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_num_entities(
         begin, SLOTS_ID, slot_size ) == size_t{ 0 } );
 
-    ASSERT_TRUE( st_BufferMem_get_section_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_num_entities(
         begin, OBJECTS_ID, slot_size ) == size_t{ 0 } );
 
-    ASSERT_TRUE( st_BufferMem_get_section_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_num_entities(
         begin, DATAPTRS_ID, slot_size ) == size_t{ 0 } );
 
-    ASSERT_TRUE( st_BufferMem_get_section_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_num_entities(
         begin, GARBAGE_ID, slot_size ) == size_t{ 0 } );
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
@@ -134,7 +134,8 @@ TEST( C99_CommonBufferMem, InitAndReserve )
     address_t cnt = address_t{ 0 };
 
     ptr_to_addr_t slot_it = reinterpret_cast< ptr_to_addr_t >(
-        st_BufferMem_get_ptr_to_section_data( begin, SLOTS_ID, slot_size ) );
+        st_ManagedBuffer_get_ptr_to_section_data(
+            begin, SLOTS_ID, slot_size ) );
 
     ptr_to_addr_t slot_end = slot_it;
     std::advance( slot_end, max_num_slots );
@@ -147,7 +148,7 @@ TEST( C99_CommonBufferMem, InitAndReserve )
         *cmp_slot_it = cnt;
     }
 
-    st_BufferMem_set_section_num_entities(
+    st_ManagedBuffer_set_section_num_entities(
         begin, SLOTS_ID, max_num_slots, slot_size );
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
@@ -159,7 +160,8 @@ TEST( C99_CommonBufferMem, InitAndReserve )
     std::vector< object_t > cmp_object_values( max_num_objects, object_t{} );
 
     ptr_to_obj_t obj_it  = reinterpret_cast< ptr_to_obj_t >(
-        st_BufferMem_get_ptr_to_section_data( begin, OBJECTS_ID, slot_size ) );
+        st_ManagedBuffer_get_ptr_to_section_data(
+            begin, OBJECTS_ID, slot_size ) );
 
     ptr_to_obj_t  obj_end = obj_it;
     std::advance( obj_end, max_num_objects );
@@ -178,21 +180,21 @@ TEST( C99_CommonBufferMem, InitAndReserve )
         *cmp_obj_it = *obj_it;
     }
 
-    st_BufferMem_set_section_num_entities(
+    st_ManagedBuffer_set_section_num_entities(
         begin, OBJECTS_ID, max_num_objects, slot_size );
 
     /* -------------------------------------------------------------------- */
 
-    size_t cur_num_objects  = st_BufferMem_get_section_num_entities(
+    size_t cur_num_objects  = st_ManagedBuffer_get_section_num_entities(
         begin, OBJECTS_ID, slot_size );
 
-    size_t cur_num_slots    = st_BufferMem_get_section_num_entities(
+    size_t cur_num_slots    = st_ManagedBuffer_get_section_num_entities(
         begin, SLOTS_ID, slot_size );
 
-    size_t cur_num_dataptrs = st_BufferMem_get_section_num_entities(
+    size_t cur_num_dataptrs = st_ManagedBuffer_get_section_num_entities(
         begin, DATAPTRS_ID, slot_size );
 
-    size_t cur_num_garbage_range = st_BufferMem_get_section_num_entities(
+    size_t cur_num_garbage_range = st_ManagedBuffer_get_section_num_entities(
         begin, GARBAGE_ID, slot_size );
 
     max_num_objects       += size_t{ 2 };
@@ -200,58 +202,61 @@ TEST( C99_CommonBufferMem, InitAndReserve )
     max_num_dataptrs      += size_t{ 4 };
     max_num_garbage_range += size_t{ 1 };
 
-    predicted_size = st_BufferMem_calculate_buffer_length( begin,
+    predicted_size = st_ManagedBuffer_calculate_buffer_length( begin,
         max_num_objects, max_num_slots, max_num_dataptrs,
             max_num_garbage_range, slot_size );
 
     ASSERT_TRUE( predicted_size > current_buffer_size );
     ASSERT_TRUE( predicted_size < data_buffer.size()  );
 
-    ASSERT_TRUE( st_BufferMem_can_reserve( begin, &current_buffer_size,
+    ASSERT_TRUE( st_ManagedBuffer_can_reserve( begin, &current_buffer_size,
         max_num_objects, max_num_slots, max_num_dataptrs,
             max_num_garbage_range, data_buffer.size(), slot_size ) );
 
-    success = st_BufferMem_reserve( begin, &current_buffer_size,
+    success = st_ManagedBuffer_reserve( begin, &current_buffer_size,
         max_num_objects, max_num_slots, max_num_dataptrs,
             max_num_garbage_range, data_buffer.size(), slot_size );
 
     ASSERT_TRUE( success == 0 );
 
-    ASSERT_TRUE( st_BufferMem_get_section_max_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_max_num_entities(
         begin, SLOTS_ID, slot_size ) == max_num_slots );
 
-    ASSERT_TRUE( st_BufferMem_get_section_max_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_max_num_entities(
         begin, OBJECTS_ID, slot_size ) == max_num_objects );
 
-    ASSERT_TRUE( st_BufferMem_get_section_max_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_max_num_entities(
         begin, DATAPTRS_ID, slot_size ) == max_num_dataptrs );
 
-    ASSERT_TRUE( st_BufferMem_get_section_max_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_max_num_entities(
         begin, GARBAGE_ID, slot_size ) == max_num_garbage_range );
 
-    ASSERT_TRUE( st_BufferMem_get_section_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_num_entities(
         begin, SLOTS_ID, slot_size ) == cur_num_slots );
 
-    ASSERT_TRUE( st_BufferMem_get_section_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_num_entities(
         begin, OBJECTS_ID, slot_size ) == cur_num_objects );
 
-    ASSERT_TRUE( st_BufferMem_get_section_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_num_entities(
         begin, DATAPTRS_ID, slot_size ) == cur_num_dataptrs );
 
-    ASSERT_TRUE( st_BufferMem_get_section_num_entities(
+    ASSERT_TRUE( st_ManagedBuffer_get_section_num_entities(
         begin, GARBAGE_ID, slot_size ) == cur_num_garbage_range );
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
     slot_it = reinterpret_cast< ptr_to_addr_t >(
-        st_BufferMem_get_ptr_to_section_data( begin, SLOTS_ID, slot_size ) );
+        st_ManagedBuffer_get_ptr_to_section_data(
+            begin, SLOTS_ID, slot_size ) );
 
     slot_end = reinterpret_cast< ptr_to_addr_t >(
-        st_BufferMem_get_ptr_to_section_end( begin, SLOTS_ID, slot_size ) );
+        st_ManagedBuffer_get_ptr_to_section_end(
+            begin, SLOTS_ID, slot_size ) );
 
     ASSERT_TRUE( std::distance( slot_it, slot_end ) ==
-        static_cast< std::ptrdiff_t >( st_BufferMem_get_section_num_entities(
-            begin, SLOTS_ID, slot_size ) ) );
+        static_cast< std::ptrdiff_t >(
+            st_ManagedBuffer_get_section_num_entities(
+                begin, SLOTS_ID, slot_size ) ) );
 
     cmp_slot_it = cmp_slot_values.begin();
 
@@ -263,14 +268,17 @@ TEST( C99_CommonBufferMem, InitAndReserve )
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
     obj_it  = reinterpret_cast< ptr_to_obj_t >(
-        st_BufferMem_get_ptr_to_section_data( begin, OBJECTS_ID, slot_size ) );
+        st_ManagedBuffer_get_ptr_to_section_data(
+            begin, OBJECTS_ID, slot_size ) );
 
     obj_end = reinterpret_cast< ptr_to_obj_t >(
-        st_BufferMem_get_ptr_to_section_end( begin, OBJECTS_ID, slot_size ) );
+        st_ManagedBuffer_get_ptr_to_section_end(
+            begin, OBJECTS_ID, slot_size ) );
 
     ASSERT_TRUE( std::distance( obj_it, obj_end ) ==
-        static_cast< std::ptrdiff_t >( st_BufferMem_get_section_num_entities(
-            begin, OBJECTS_ID, slot_size ) ) );
+        static_cast< std::ptrdiff_t >(
+            st_ManagedBuffer_get_section_num_entities(
+                begin, OBJECTS_ID, slot_size ) ) );
 
     cmp_obj_it = cmp_object_values.begin();
 
@@ -288,4 +296,4 @@ TEST( C99_CommonBufferMem, InitAndReserve )
 }
 
 /* ------------------------------------------------------------------------- */
-/* end: tests/sixtracklib/common/test_buffer_mem.cpp */
+/* end: tests/sixtracklib/common/test_managed_buffer_c99.cpp */
