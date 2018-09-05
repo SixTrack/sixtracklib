495170c366ca5770e04e6d5c22f3d7acedeb4644
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-09-05 13:51:14 +0200

    tests/testlib: Add helper functions for various beam-elements during unit-tests and benchmarks
    - Add printing functions for the so far implemented beam elements. These functions
      use printf and should, assuming availability of the printf command, also be
      usable from any GPU kernels for debugging and documentation purposes.
    
    - Add a second set of printing functions printing to a FILE* descriptor.
      They are host-only code and rely on the availability of fprintf command.

--- tests/sixtracklib/testlib.h
+++ tests/sixtracklib/testlib.h
@@ -14,6 +14,7 @@
 #include "sixtracklib/testlib/gpu_kernel_tools.h"
 #include "sixtracklib/testlib/random.h"
 #include "sixtracklib/testlib/test_particles_tools.h"
+#include "sixtracklib/testlib/test_beam_elements_tools.h"
 // #include "sixtracklib/testlib/test_track_tools.h"
 
 #include "sixtracklib/testlib/testdata_files.h"
