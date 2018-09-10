6dc99eeb499654f0da636fb9e4d1e31c1cfb1552
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-09-10 19:50:33 +0200

    sixtracklib/common: Update NS(Drift) and NS(DriftExact) as test for the new memory region decorator scheme

--- sixtracklib/common/impl/be_drift.h
+++ sixtracklib/common/impl/be_drift.h
@@ -9,6 +9,7 @@
 
 #if !defined( SIXTRL_NO_INCLUDES )
     #include "sixtracklib/_impl/definitions.h"
+    #include "sixtracklib/common/impl/buffer_defines.h"
     #include "sixtracklib/common/impl/beam_elements_defines.h"
     #include "sixtracklib/common/impl/be_drift_defines.h"
     #include "sixtracklib/common/impl/buffer_type.h"
@@ -48,16 +49,16 @@ SIXTRL_FN SIXTRL_STATIC void NS(Drift_set_length)(
 #if !defined( _GPUCODE )
 
 SIXTRL_FN SIXTRL_STATIC bool NS(Drift_can_be_added)(
-    SIXTRL_BE_BUFFER_ARGPTR_DEC const NS(Buffer) *const SIXTRL_RESTRICT buffer,
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT ptr_requ_objects,
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT ptr_requ_slots,
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT ptr_requ_dataptrs );
+    SIXTRL_BUFFER_ARGPTR_DEC const NS(Buffer) *const SIXTRL_RESTRICT buffer,
+    SIXTRL_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT ptr_requ_objects,
+    SIXTRL_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT ptr_requ_slots,
+    SIXTRL_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT ptr_requ_dataptrs );
 
-SIXTRL_FN SIXTRL_STATIC SIXTRL_BE_BUFFER_DATAPTR_DEC NS(Drift)* NS(Drift_new)(
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer );
+SIXTRL_FN SIXTRL_STATIC SIXTRL_BUFFER_DATAPTR_DEC NS(Drift)* NS(Drift_new)(
+    SIXTRL_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer );
 
-SIXTRL_FN SIXTRL_STATIC SIXTRL_BE_BUFFER_DATAPTR_DEC NS(Drift)* NS(Drift_add)(
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer,
+SIXTRL_FN SIXTRL_STATIC SIXTRL_BUFFER_DATAPTR_DEC NS(Drift)* NS(Drift_add)(
+    SIXTRL_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer,
     NS(drift_real_t) const length );
 
 #endif /* !defined( _GPUCODE ) */
@@ -91,19 +92,18 @@ SIXTRL_FN SIXTRL_STATIC void NS(DriftExact_set_length)(
 #if !defined( _GPUCODE )
 
 SIXTRL_FN SIXTRL_STATIC bool NS(DriftExact_can_be_added)(
-    SIXTRL_BE_BUFFER_ARGPTR_DEC const NS(Buffer) *const SIXTRL_RESTRICT buffer,
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_objects,
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_slots,
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_dataptrs );
+    SIXTRL_BUFFER_ARGPTR_DEC const NS(Buffer) *const SIXTRL_RESTRICT buffer,
+    SIXTRL_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_objects,
+    SIXTRL_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_slots,
+    SIXTRL_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_dataptrs );
 
-SIXTRL_FN SIXTRL_STATIC SIXTRL_BE_BUFFER_DATAPTR_DEC NS(DriftExact)*
-NS(DriftExact_new)( SIXTRL_BE_BUFFER_ARGPTR_DEC
+SIXTRL_FN SIXTRL_STATIC SIXTRL_BUFFER_DATAPTR_DEC NS(DriftExact)*
+NS(DriftExact_new)( SIXTRL_BUFFER_ARGPTR_DEC
     NS(Buffer)* SIXTRL_RESTRICT buffer );
 
-SIXTRL_FN SIXTRL_STATIC SIXTRL_BE_BUFFER_DATAPTR_DEC NS(DriftExact)*
-NS(DriftExact_add)(
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer,
-    NS(drift_real_t) const length );
+SIXTRL_FN SIXTRL_STATIC SIXTRL_BUFFER_DATAPTR_DEC NS(DriftExact)*
+NS(DriftExact_add)( SIXTRL_BUFFER_ARGPTR_DEC NS(Buffer)*
+    SIXTRL_RESTRICT buffer, NS(drift_real_t) const length );
 
 #endif /* !defined( _GPUCODE ) */
 
@@ -162,10 +162,10 @@ SIXTRL_INLINE void NS(Drift_set_length)(
 #if !defined( _GPUCODE )
 
 SIXTRL_INLINE  bool NS(Drift_can_be_added)(
-    SIXTRL_BE_BUFFER_ARGPTR_DEC const NS(Buffer) *const SIXTRL_RESTRICT buffer,
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_objects,
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_slots,
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_dataptrs )
+    SIXTRL_BUFFER_ARGPTR_DEC const NS(Buffer) *const SIXTRL_RESTRICT buffer,
+    SIXTRL_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_objects,
+    SIXTRL_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_slots,
+    SIXTRL_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_dataptrs )
 {
     typedef NS(Drift)           elem_t;
     typedef NS(buffer_size_t)   buf_size_t;
@@ -173,8 +173,8 @@ SIXTRL_INLINE  bool NS(Drift_can_be_added)(
     buf_size_t const num_dataptrs =
         NS(DriftExact_get_num_dataptrs)( SIXTRL_NULLPTR );
 
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* sizes   = SIXTRL_NULLPTR;
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* counts  = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* sizes   = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* counts  = SIXTRL_NULLPTR;
 
     SIXTRL_ASSERT( num_dataptrs == ( buf_size_t )0u );
 
@@ -182,19 +182,19 @@ SIXTRL_INLINE  bool NS(Drift_can_be_added)(
         sizes, counts, requ_objects, requ_slots, requ_dataptrs );
 }
 
-SIXTRL_INLINE SIXTRL_BE_BUFFER_DATAPTR_DEC NS(Drift)* NS(Drift_new)(
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer )
+SIXTRL_INLINE SIXTRL_BUFFER_DATAPTR_DEC NS(Drift)* NS(Drift_new)(
+    SIXTRL_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer )
 {
-    typedef NS(Drift)                               elem_t;
-    typedef SIXTRL_BE_BUFFER_DATAPTR_DEC elem_t*    ptr_to_elem_t;
-    typedef NS(buffer_size_t)                       buf_size_t;
+    typedef NS(Drift)                            elem_t;
+    typedef SIXTRL_BUFFER_DATAPTR_DEC elem_t*    ptr_to_elem_t;
+    typedef NS(buffer_size_t)                    buf_size_t;
 
     buf_size_t const num_dataptrs =
         NS(DriftExact_get_num_dataptrs)( SIXTRL_NULLPTR );
 
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* offsets = SIXTRL_NULLPTR;
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* sizes   = SIXTRL_NULLPTR;
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* counts  = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* offsets = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* sizes   = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* counts  = SIXTRL_NULLPTR;
 
     elem_t temp_obj;
     temp_obj.length = ( NS(drift_real_t) )0.0;
@@ -206,20 +206,20 @@ SIXTRL_INLINE SIXTRL_BE_BUFFER_DATAPTR_DEC NS(Drift)* NS(Drift_new)(
             NS(OBJECT_TYPE_DRIFT), num_dataptrs, offsets, sizes, counts ) );
 }
 
-SIXTRL_INLINE SIXTRL_BE_BUFFER_DATAPTR_DEC NS(Drift)* NS(Drift_add)(
-    SIXTRL_BE_BUFFER_ARGPTR_DEC  NS(Buffer)* SIXTRL_RESTRICT buffer,
+SIXTRL_INLINE SIXTRL_BUFFER_DATAPTR_DEC NS(Drift)* NS(Drift_add)(
+    SIXTRL_BUFFER_ARGPTR_DEC  NS(Buffer)* SIXTRL_RESTRICT buffer,
     NS(drift_real_t) const length )
 {
-    typedef NS(Drift)                               elem_t;
-    typedef SIXTRL_BE_BUFFER_DATAPTR_DEC elem_t*    ptr_to_elem_t;
-    typedef NS(buffer_size_t)                       buf_size_t;
+    typedef NS(Drift)                            elem_t;
+    typedef SIXTRL_BUFFER_DATAPTR_DEC elem_t*    ptr_to_elem_t;
+    typedef NS(buffer_size_t)                    buf_size_t;
 
     buf_size_t const num_dataptrs =
         NS(DriftExact_get_num_dataptrs)( SIXTRL_NULLPTR );
 
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* offsets = SIXTRL_NULLPTR;
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* sizes   = SIXTRL_NULLPTR;
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* counts  = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* offsets = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* sizes   = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* counts  = SIXTRL_NULLPTR;
 
     elem_t temp_obj;
     temp_obj.length = length;
@@ -272,18 +272,18 @@ SIXTRL_INLINE void NS(DriftExact_set_length)(
 #if !defined( _GPUCODE )
 
 SIXTRL_INLINE  bool NS(DriftExact_can_be_added)(
-    SIXTRL_BE_BUFFER_ARGPTR_DEC const NS(Buffer) *const SIXTRL_RESTRICT buffer,
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_objects,
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_slots,
-    SIXTRL_BE_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_dataptrs )
+    SIXTRL_BUFFER_ARGPTR_DEC const NS(Buffer) *const SIXTRL_RESTRICT buffer,
+    SIXTRL_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_objects,
+    SIXTRL_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_slots,
+    SIXTRL_BUFFER_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT requ_dataptrs )
 {
     typedef NS(buffer_size_t) buf_size_t;
 
     buf_size_t const num_dataptrs =
         NS(DriftExact_get_num_dataptrs)( SIXTRL_NULLPTR );
 
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* sizes  = SIXTRL_NULLPTR;
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* counts = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* sizes  = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* counts = SIXTRL_NULLPTR;
 
     SIXTRL_ASSERT( num_dataptrs == ( buf_size_t )0u );
 
@@ -291,20 +291,20 @@ SIXTRL_INLINE  bool NS(DriftExact_can_be_added)(
         num_dataptrs, sizes, counts, requ_objects, requ_slots, requ_dataptrs );
 }
 
-SIXTRL_INLINE SIXTRL_BE_BUFFER_DATAPTR_DEC NS(DriftExact)*
-NS(DriftExact_new)( SIXTRL_BE_BUFFER_ARGPTR_DEC NS(Buffer)*
+SIXTRL_INLINE SIXTRL_BUFFER_DATAPTR_DEC NS(DriftExact)*
+NS(DriftExact_new)( SIXTRL_BUFFER_ARGPTR_DEC NS(Buffer)*
     SIXTRL_RESTRICT buffer )
 {
-    typedef NS(DriftExact)                         elem_t;
-    typedef SIXTRL_BE_BUFFER_DATAPTR_DEC elem_t*   ptr_to_elem_t;
-    typedef NS(buffer_size_t)                      buf_size_t;
+    typedef NS(DriftExact)                      elem_t;
+    typedef SIXTRL_BUFFER_DATAPTR_DEC elem_t*   ptr_to_elem_t;
+    typedef NS(buffer_size_t)                   buf_size_t;
 
     buf_size_t const num_dataptrs =
         NS(DriftExact_get_num_dataptrs)( SIXTRL_NULLPTR );
 
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* offsets = SIXTRL_NULLPTR;
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* sizes   = SIXTRL_NULLPTR;
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* counts  = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* offsets = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* sizes   = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* counts  = SIXTRL_NULLPTR;
 
     elem_t temp_obj;
     temp_obj.length = ( NS(drift_real_t) )0.0;
@@ -317,21 +317,21 @@ NS(DriftExact_new)( SIXTRL_BE_BUFFER_ARGPTR_DEC NS(Buffer)*
                 offsets, sizes, counts ) );
 }
 
-SIXTRL_INLINE  SIXTRL_BE_BUFFER_DATAPTR_DEC NS(DriftExact)*
+SIXTRL_INLINE  SIXTRL_BUFFER_DATAPTR_DEC NS(DriftExact)*
     NS(DriftExact_add)(
-        SIXTRL_BE_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer,
+        SIXTRL_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer,
         NS(drift_real_t) const length )
 {
-    typedef NS(DriftExact)                         elem_t;
-    typedef SIXTRL_BE_BUFFER_DATAPTR_DEC elem_t*   ptr_to_elem_t;
-    typedef NS(buffer_size_t)                      buf_size_t;
+    typedef NS(DriftExact)                      elem_t;
+    typedef SIXTRL_BUFFER_DATAPTR_DEC elem_t*   ptr_to_elem_t;
+    typedef NS(buffer_size_t)                   buf_size_t;
 
     buf_size_t const num_dataptrs =
         NS(DriftExact_get_num_dataptrs)( SIXTRL_NULLPTR );
 
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* offsets = SIXTRL_NULLPTR;
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* sizes   = SIXTRL_NULLPTR;
-    SIXTRL_BE_BUFFER_ARGPTR_DEC buf_size_t const* counts  = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* offsets = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* sizes   = SIXTRL_NULLPTR;
+    SIXTRL_BUFFER_ARGPTR_DEC buf_size_t const* counts  = SIXTRL_NULLPTR;
 
     elem_t temp_obj;
     temp_obj.length = length;
--- sixtracklib/common/impl/beam_elements_defines.h
+++ sixtracklib/common/impl/beam_elements_defines.h
@@ -3,7 +3,7 @@
 
 #if !defined( SIXTRL_BE_ARGPTR_DEC )
     #define SIXTRL_BE_ARGPTR_DEC_UNDEF
-    #if defined( SIXTRL_ARGPTR_DEC )
+    #if defined( SIXTRL_BUFFER_DATAPTR_ARGPTR_DEC )
         #define  SIXTRL_BE_ARGPTR_DEC SIXTRL_ARGPTR_DEC
     #else /* defined( SIXTRL_ARGPTR_DEC ) */
         #define  SIXTRL_BE_ARGPTR_DEC
@@ -19,23 +19,7 @@
     #endif /* defined( SIXTRL_DATAPTR_DEC ) */
 #endif /* !defined( SIXTRL_BE_DATAPTR_DEC ) */
 
-#if !defined( SIXTRL_BE_BUFFER_ARGPTR_DEC )
-    #define SIXTRL_BE_BUFFER_ARGPTR_DEC_UNDEF
-    #if defined( SIXTRL_ARGPTR_DEC )
-        #define SIXTRL_BE_BUFFER_ARGPTR_DEC SIXTRL_ARGPTR_DEC
-    #else /* defined( SIXTRL_ARGPTR_DEC ) */
-        #define SIXTRL_BE_BUFFER_ARGPTR_DEC
-    #endif /* defined( SIXTRL_ARGPTR_DEC ) */
-#endif /* !defined( SIXTRL_BE_BUFFER_ARGPTR_DEC ) */
 
-#if !defined( SIXTRL_BE_BUFFER_DATAPTR_DEC )
-    #define SIXTRL_BE_BUFFER_DATAPTR_DEC_UNDEF
-    #if defined( SIXTRL_DATAPTR_DEC )
-        #define SIXTRL_BE_BUFFER_DATAPTR_DEC SIXTRL_DATAPTR_DEC
-    #else /* defined( SIXTRL_DATAPTR_DEC ) */
-        #define SIXTRL_BE_BUFFER_DATAPTR_DEC
-    #endif /* defined( SIXTRL_DATAPTR_DEC ) */
-#endif /* !defined( SIXTRL_BE_BUFFER_DATAPTR_DEC ) */
 
 #endif /* SIXTRACKLIB_COMMON_IMPL_BEAM_ELEMENTS_DEFINES_H__ */
 
