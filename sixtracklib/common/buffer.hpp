0dbb14e4afff3d3eb15216c3bfc069a3dab7387f
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-10-02 20:37:38 +0200

    sixtracklib/common: add host-only member functions for initializing a C++ Buffer from existing binary dump files

--- sixtracklib/common/buffer.hpp
+++ sixtracklib/common/buffer.hpp
@@ -6,8 +6,10 @@
         #include <cstddef>
         #include <cstdint>
         #include <cstdlib>
+        #include <cstdio>
         #include <limits>
         #include <utility>
+        #include <string>
     #endif /* defined( __cplusplus ) */
 #endif /* !defined( SIXTRL_NO_SYSTEM_INCLUDES ) */
 
@@ -68,6 +70,11 @@ namespace SIXTRL_NAMESPACE
             size_type max_num_dataptrs,
             size_type max_num_garbage_ranges,
             size_type buffer_capacity ) SIXTRL_NOEXCEPT;
+
+        #if !defined( GPUCODE )
+        SIXTRL_HOST_FN Buffer( char const* path_to_file );
+        SIXTRL_HOST_FN Buffer( std::string const& path_to_file );
+        #endif /* !defined( GPUCODE ) */
 
         /* TODO: Implement Copy & Move semantics for Buffer */
         SIXTRL_FN Buffer( Buffer const& other ) = delete;
@@ -229,6 +236,13 @@ namespace SIXTRL_NAMESPACE
 
         private:
 
+        #if !defined( _GPUCODE )
+
+        SIXTRL_HOST_FN SIXTRL_STATIC size_type GetBinaryFileSize(
+            char const* path_to_file ) SIXTRL_NOEXCEPT;
+
+        #endif /* !defined( _GPUCODE ) */
+
         bool allocateDataStore(
             size_type const buffer_capacity, flags_t const flags );
     };
@@ -355,6 +369,50 @@ namespace SIXTRL_NAMESPACE
             ::NS(Buffer_free)( _buffer );
         }
     }
+
+    #if !defined( GPUCODE )
+    SIXTRL_INLINE Buffer::Buffer( char const* path_to_file ) : ::NS(Buffer)()
+    {
+        using c_api_t = Buffer::c_api_t;
+        using size_t  = Buffer::size_type;
+
+        size_t const required_buffer_size =
+            Buffer::GetBinaryFileSize( path_to_file );
+
+        c_api_t* _buffer = ::NS(Buffer_preset)( this->getCApiPtr() );
+
+        if( this->allocateDataStore( required_buffer_size,
+                                     Buffer::DEFAULT_DATASTORE_FLAGS ) )
+        {
+            ::NS(Buffer_read_from_file)( _buffer, path_to_file );
+        }
+        else
+        {
+            ::NS(Buffer_free)( _buffer );
+        }
+    }
+
+    SIXTRL_INLINE Buffer::Buffer( std::string const& path_to_file ) : ::NS(Buffer)()
+    {
+        using c_api_t = Buffer::c_api_t;
+        using size_t  = Buffer::size_type;
+
+        size_t const required_buffer_size =
+            Buffer::GetBinaryFileSize( path_to_file.c_str() );
+
+        c_api_t* _buffer = ::NS(Buffer_preset)( this->getCApiPtr() );
+
+        if( this->allocateDataStore( required_buffer_size,
+                                     Buffer::DEFAULT_DATASTORE_FLAGS ) )
+        {
+            ::NS(Buffer_read_from_file)( _buffer, path_to_file.c_str() );
+        }
+        else
+        {
+            ::NS(Buffer_free)( _buffer );
+        }
+    }
+    #endif /* !defined( _GPUCODE ) */
 
     SIXTRL_INLINE Buffer::~Buffer()
     {
@@ -791,6 +849,34 @@ namespace SIXTRL_NAMESPACE
             reinterpret_cast< ptr_const_size_t >( sizes   ),
             reinterpret_cast< ptr_const_size_t >( counts  ) );
     }
+
+    #if !defined( _GPUCODE )
+    SIXTRL_HOST_FN SIXTRL_INLINE Buffer::size_type
+    Buffer::GetBinaryFileSize( char const* path_to_file ) SIXTRL_NOEXCEPT
+    {
+        using size_t = Buffer::size_type;
+
+        size_t file_size = size_t{ 0 };
+        FILE* fp = std::fopen( path_to_file, "rb" );
+
+        if( fp != nullptr )
+        {
+            long length = long{ 0u };
+
+            std::fseek( fp, 0, SEEK_END );
+            length = std::ftell( fp );
+            std::fclose( fp );
+            fp = nullptr;
+
+            if( length > long{ 0 } )
+            {
+                file_size = static_cast< size_t >( length );
+            }
+        }
+
+        return file_size;
+    }
+    #endif /* !defined( _GPUCODE ) */
 
     SIXTRL_INLINE bool Buffer::allocateDataStore(
         Buffer::size_type const buffer_capacity, Buffer::flags_t const flags )
