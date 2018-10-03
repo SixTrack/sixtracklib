833aff66e39ed0f3fa264c8a7de142f5dd4892eb
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-10-04 00:57:20 +0200

    tests/testlib: updat testlib.h convenience header after refactoring

--- tests/sixtracklib/testlib.h
+++ tests/sixtracklib/testlib.h
@@ -1,15 +1,8 @@
 #ifndef SIXTRACKLIB_TESTS_SIXTRACKLIB_TESTLIB_H__
 #define SIXTRACKLIB_TESTS_SIXTRACKLIB_TESTLIB_H__
 
-#if !defined( __NAMESPACE )
-    #define __NAMESPACE st_
-    #define __SIXTRACKLIB_TESTS_SIXTRACKLIB_UNDEF_NAMESPACE 1
-#endif /* !defined( __NAMESPACE ) */
-
-/* ------------------------------------------------------------------------- */
-
-#include "sixtracklib/_impl/definitions.h"
-#include "sixtracklib/_impl/namespace_begin.h"
+#include "sixtracklib/common/generated/namespace.h"
+#include "sixtracklib/common/definitions.h"
 #include "sixtracklib/testlib/generic_buffer_obj.h"
 #include "sixtracklib/testlib/gpu_kernel_tools.h"
 #include "sixtracklib/testlib/random.h"
@@ -17,8 +10,6 @@
 #include "sixtracklib/testlib/test_particles_tools.h"
 #include "sixtracklib/testlib/test_beam_elements_tools.h"
 #include "sixtracklib/testlib/track_testdata_tools.h"
-// #include "sixtracklib/testlib/test_track_tools.h"
-
 #include "sixtracklib/testlib/testdata_files.h"
 
 /* ------------------------------------------------------------------------- */
