76775ae8394fc9e32e416bca8ffee5a5bc779e0d
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-10-04 15:54:01 +0200

    sixtracklib: remove trailing undef section from the convienience header -> provide a dedicated undef header for this!

--- sixtracklib/sixtracklib.h
+++ sixtracklib/sixtracklib.h
@@ -72,13 +72,6 @@
 
 /* ------------------------------------------------------------------------- */
 
-#if defined( SIXTRACKLIB_SIXTRACKLIB_UNDEF_NAMESPACE )
-    #include "sixtracklib/_impl/namespace_end.h"
-
-    #undef __NAMESPACE
-    #undef __SIXTRACKLIB_SIXTRACKLIB_UNDEF_NAMESPACE
-#endif /* defined( SIXTRACKLIB_SIXTRACKLIB_UNDEF_NAMESPACE ) */
-
 #endif /* SIXTRACKLIB_SIXTRACKLIB_H__ */
 
 /* end: sixtracklib/sixtracklib.h */
