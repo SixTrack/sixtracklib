f782558ae835d7521b1c17df36ec19bf65fa4644
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-10-27 09:29:45 +0200

    tests/sixtracklib/python: prepare introducing python based examples using pysixtracklib (among other packages)

---
+++ examples/python/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cobjects
+import pysixtrack
+
+import particles as tp
+import export_to_cobjects as etc
---
+++ examples/python/track_simplebb.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cobjects
+
+# For the moment, we use PySixTrack for the actual tracking in this example;
+# eventually, this should be rewritten to use pysixtracklib instead
+
+import pysixtrack
+import pysixtracklib
+
+if __name__ == '__main__':
+
+
