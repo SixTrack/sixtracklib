665ff96980ff1cd24b1ae8fdf5c6516d23d0a0e4
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-11-06 11:43:06 +0100

    tests/sixtracklib/testlib/common: Update beam elements helper functions
    - Add (proper) support for printing NS(BeamBeam4D), NS(BeamBeam6D) and
      NS(BeamMonitor) elements
    
    - Refactor the functions and unify the naming scheme
    - Use proper memory region decorators in the beam elements related printing
      functions
    
    - Provide a C-wrapper interface not only for the fprintf functions but
      also for the printf based functions -> these should help providing
      python bindings for the whole testlib eventually

--- tests/sixtracklib/testlib/common/beam_elements.c
+++ tests/sixtracklib/testlib/common/beam_elements.c
@@ -12,41 +12,148 @@
 
 /* ------------------------------------------------------------------------- */
 
-extern SIXTRL_HOST_FN void NS(Drift_fprint)(
-    FILE* SIXTRL_RESTRICT fp,
-    SIXTRL_ARGPTR_DEC const NS(Drift) *const SIXTRL_RESTRICT drift );
+extern SIXTRL_HOST_FN void NS(Drift_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(Drift) *const SIXTRL_RESTRICT drift );
 
-extern SIXTRL_HOST_FN void NS(DriftExact_fprint)(
-    FILE* SIXTRL_RESTRICT fp,
-    SIXTRL_ARGPTR_DEC const NS(DriftExact) *const SIXTRL_RESTRICT drift );
+extern SIXTRL_HOST_FN void NS(DriftExact_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(DriftExact) *const SIXTRL_RESTRICT drift );
 
-extern SIXTRL_HOST_FN void NS(MultiPole_fprint)(
-    FILE* SIXTRL_RESTRICT fp,
-    SIXTRL_ARGPTR_DEC const NS(MultiPole) *const SIXTRL_RESTRICT mp );
+extern SIXTRL_HOST_FN void NS(MultiPole_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(MultiPole) *const SIXTRL_RESTRICT mp );
 
-extern SIXTRL_HOST_FN void NS(XYShift_fprint)(
-    FILE* SIXTRL_RESTRICT fp,
-    SIXTRL_ARGPTR_DEC const NS(XYShift) *const SIXTRL_RESTRICT xy_shift );
+extern SIXTRL_HOST_FN void NS(XYShift_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(XYShift) *const SIXTRL_RESTRICT xy_shift );
 
-extern SIXTRL_HOST_FN void NS(SRotation_fprint)(
-    FILE* SIXTRL_RESTRICT fp,
-    SIXTRL_ARGPTR_DEC const NS(SRotation) *const SIXTRL_RESTRICT srot );
+extern SIXTRL_HOST_FN void NS(SRotation_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(SRotation) *const SIXTRL_RESTRICT srot );
 
-extern SIXTRL_HOST_FN void NS(Cavity_fprint)(
-    FILE* SIXTRL_RESTRICT fp,
-    SIXTRL_ARGPTR_DEC const NS(Cavity) *const SIXTRL_RESTRICT cav );
+extern SIXTRL_HOST_FN void NS(Cavity_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(Cavity) *const SIXTRL_RESTRICT cav );
+
+extern SIXTRL_HOST_FN void NS(BeamBeam4D_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(BeamBeam4D) *const SIXTRL_RESTRICT bb4d );
+
+extern SIXTRL_HOST_FN void NS(BeamBeam6D_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(BeamBeam6D) *const SIXTRL_RESTRICT bb6d );
+
+extern SIXTRL_HOST_FN void NS(BeamMonitor_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(BeamMonitor) *const SIXTRL_RESTRICT monitor );
+
+
+extern SIXTRL_HOST_FN void NS(BeamElement_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(Object) *const SIXTRL_RESTRICT be_info );
 
 /* ------------------------------------------------------------------------- */
 
+extern SIXTRL_HOST_FN void NS(Drift_fprint)( FILE* SIXTRL_RESTRICT fp,
+    SIXTRL_ARGPTR_DEC const NS(Drift) *const SIXTRL_RESTRICT drift );
+
+extern SIXTRL_HOST_FN void NS(DriftExact_fprint)( FILE* SIXTRL_RESTRICT fp,
+    SIXTRL_ARGPTR_DEC const NS(DriftExact) *const SIXTRL_RESTRICT drift );
+
+extern SIXTRL_HOST_FN void NS(MultiPole_fprint)( FILE* SIXTRL_RESTRICT fp,
+    SIXTRL_ARGPTR_DEC const NS(MultiPole) *const SIXTRL_RESTRICT mp );
+
+extern SIXTRL_HOST_FN void NS(XYShift_fprint)( FILE* SIXTRL_RESTRICT fp,
+    SIXTRL_ARGPTR_DEC const NS(XYShift) *const SIXTRL_RESTRICT xy_shift );
+
+extern SIXTRL_HOST_FN void NS(SRotation_fprint)( FILE* SIXTRL_RESTRICT fp,
+    SIXTRL_ARGPTR_DEC const NS(SRotation) *const SIXTRL_RESTRICT srot );
+
+extern SIXTRL_HOST_FN void NS(Cavity_fprint)( FILE* SIXTRL_RESTRICT fp,
+    SIXTRL_ARGPTR_DEC const NS(Cavity) *const SIXTRL_RESTRICT cav );
+
+extern SIXTRL_HOST_FN void NS(BeamBeam4D_fprint)( FILE* SIXTRL_RESTRICT fp,
+    SIXTRL_BE_ARGPTR_DEC const NS(BeamBeam4D) *const SIXTRL_RESTRICT bb4d );
+
+extern SIXTRL_HOST_FN void NS(BeamBeam6D_fprint)( FILE* SIXTRL_RESTRICT fp,
+    SIXTRL_BE_ARGPTR_DEC const NS(BeamBeam6D) *const SIXTRL_RESTRICT bb6d );
+
+extern SIXTRL_HOST_FN void NS(BeamMonitor_fprint)( FILE* SIXTRL_RESTRICT fp,
+    SIXTRL_BE_ARGPTR_DEC const NS(BeamMonitor) *const SIXTRL_RESTRICT monitor );
+
+
 extern SIXTRL_HOST_FN void NS(BeamElement_fprint)( FILE* SIXTRL_RESTRICT fp,
-    SIXTRL_ARGPTR_DEC const NS(Object) *const SIXTRL_RESTRICT be_info );
+    SIXTRL_BE_ARGPTR_DEC  const NS(Object) *const SIXTRL_RESTRICT be_info );
 
 /* ========================================================================= */
 /* =====           BEAM_ELEMENTS_TOOLS IMPLEMENTATION                  ===== */
 /* ========================================================================= */
 
-void NS(Drift_fprint)(
-    FILE* SIXTRL_RESTRICT fp,
+void NS(Drift_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(Drift) *const SIXTRL_RESTRICT drift )
+{
+    NS(Drift_print)( drift );
+    return;
+}
+
+void NS(DriftExact_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(DriftExact) *const SIXTRL_RESTRICT drift )
+{
+    NS(DriftExact_print)( drift );
+    return;
+}
+
+void NS(MultiPole_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(MultiPole) *const SIXTRL_RESTRICT mp )
+{
+    NS(MultiPole_print)( mp );
+    return;
+}
+
+void NS(XYShift_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(XYShift) *const SIXTRL_RESTRICT xy_shift )
+{
+    NS(XYShift_print)( xy_shift );
+    return;
+}
+
+void NS(SRotation_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(SRotation) *const SIXTRL_RESTRICT srot )
+{
+    NS(SRotation_print)( srot );
+    return;
+}
+
+void NS(Cavity_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(Cavity) *const SIXTRL_RESTRICT cav )
+{
+    NS(Cavity_print)( cav );
+    return;
+}
+
+void NS(BeamBeam4D_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(BeamBeam4D) *const SIXTRL_RESTRICT bb4d )
+{
+    NS(BeamBeam4D_print)( bb4d );
+    return;
+}
+
+void NS(BeamBeam6D_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(BeamBeam6D) *const SIXTRL_RESTRICT bb6d )
+{
+    NS(BeamBeam6D_print)( bb6d );
+    return;
+}
+
+void NS(BeamMonitor_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(BeamMonitor) *const SIXTRL_RESTRICT monitor )
+{
+    NS(BeamMonitor_print)( monitor );
+    return;
+}
+
+
+void NS(BeamElement_print_out)( SIXTRL_BE_ARGPTR_DEC const
+    NS(Object) *const SIXTRL_RESTRICT be_info )
+{
+    NS(BeamElement_print)( be_info );
+    return;
+}
+
+/* ========================================================================= */
+
+void NS(Drift_fprint)( FILE* SIXTRL_RESTRICT fp,
     SIXTRL_ARGPTR_DEC const NS(Drift) *const SIXTRL_RESTRICT drift )
 {
     SIXTRL_ASSERT( fp != SIXTRL_NULLPTR );
@@ -152,6 +259,133 @@ void NS(Cavity_fprint)(
     return;
 }
 
+
+void NS(BeamBeam4D_fprint)( FILE* SIXTRL_RESTRICT fp,
+    SIXTRL_BE_ARGPTR_DEC const NS(BeamBeam4D) *const SIXTRL_RESTRICT bb4d )
+{
+    typedef NS(beambeam4d_real_const_ptr_t)  bb_data_ptr_t;
+    typedef SIXTRL_BE_DATAPTR_DEC BB4D_data* BB4D_data_ptr_t;
+
+    bb_data_ptr_t data = NS(BeamBeam4D_get_const_data)( bb4d );
+    BB4D_data_ptr_t bb4ddata = (BB4D_data_ptr_t) data;
+
+    SIXTRL_ASSERT( bb4ddata != SIXTRL_NULLPTR );
+
+    fprintf( fp,
+            "|beambeam4d      | q_part         = %+20e\r\n"
+            "                 | N_part         = %+20e\r\n"
+            "                 | sigma_x        = %+20.12f\r\n"
+            "                 | sigma_y        = %+20.12f\r\n"
+            "                 | beta_s         = %+20.12f\r\n"
+            "                 | min_sigma_diff = %+20.12f\r\n"
+            "                 | Delta_x        = %+20.12f\r\n"
+            "                 | Delta_y        = %+20.12f\r\n"
+            "                 | Dpx_sub        = %+20.12f\r\n"
+            "                 | Dpy_sub        = %+20.12f\r\n"
+            "                 | enabled        = %20ld\r\n",
+            bb4ddata->q_part,  bb4ddata->N_part,  bb4ddata->sigma_x,
+            bb4ddata->sigma_y, bb4ddata->beta_s,  bb4ddata->min_sigma_diff,
+            bb4ddata->Delta_x, bb4ddata->Delta_y, bb4ddata->Dpx_sub,
+            bb4ddata->Dpy_sub, ( long int )bb4ddata->enabled );
+
+    return;
+}
+
+void NS(BeamBeam6D_fprint)(
+    FILE* SIXTRL_RESTRICT fp,
+    SIXTRL_BE_ARGPTR_DEC const NS(BeamBeam6D) *const SIXTRL_RESTRICT bb6d )
+{
+    typedef SIXTRL_REAL_T                       real_t;
+    typedef NS(beambeam6d_real_const_ptr_t)     bb_data_ptr_t;
+    typedef SIXTRL_BE_DATAPTR_DEC BB6D_data*    BB6D_data_ptr_t;
+    typedef SIXTRL_BE_DATAPTR_DEC real_t const* ptr_real_t;
+
+    bb_data_ptr_t data = NS(BeamBeam6D_get_const_data)( bb6d );
+    BB6D_data_ptr_t bb6ddata = (BB6D_data_ptr_t) data;
+
+    if( ( bb6ddata != SIXTRL_NULLPTR ) && ( bb6ddata->enabled ) )
+    {
+        int num_slices = (int)(bb6ddata->N_slices);
+        int ii = 0;
+
+        ptr_real_t N_part_per_slice =
+            SIXTRL_BB_GET_PTR(bb6ddata, N_part_per_slice);
+
+        ptr_real_t x_slices_star =
+            SIXTRL_BB_GET_PTR(bb6ddata, x_slices_star);
+
+        ptr_real_t y_slices_star =
+            SIXTRL_BB_GET_PTR(bb6ddata, y_slices_star);
+
+        ptr_real_t sigma_slices_star =
+            SIXTRL_BB_GET_PTR(bb6ddata, sigma_slices_star);
+
+        SIXTRL_ASSERT( N_part_per_slice  != SIXTRL_NULLPTR );
+        SIXTRL_ASSERT( x_slices_star     != SIXTRL_NULLPTR );
+        SIXTRL_ASSERT( y_slices_star     != SIXTRL_NULLPTR );
+        SIXTRL_ASSERT( sigma_slices_star != SIXTRL_NULLPTR );
+
+        fprintf( fp,
+                "|beambeam6d      | enabled                = %20ld\r\n"
+                "                 | sphi                   = %+20e\r\n"
+                "                 | calpha                 = %+20e\r\n"
+                "                 | S33                    = %+20.12f\r\n"
+                "                 | N_slices               = %+20d\r\n",
+                ( long int )bb6ddata->enabled,
+                (bb6ddata->parboost).sphi, (bb6ddata->parboost).calpha,
+                (bb6ddata->Sigmas_0_star).Sig_33_0, num_slices );
+
+        for( ; ii < num_slices ; ++ii )
+        {
+            fprintf( fp,
+                    ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . "
+                    ". . . . . . . . . . . . . . . . . . . . . . . . . . . . \r\n"
+                    "                 | N_part_per_slice[%4d]  = %20e\r\n"
+                    "                 | x_slices_star[%4d]     = %20.12f\r\n"
+                    "                 | y_slices_star[%4d]     = %20.12f\r\n"
+                    "                 | sigma_slices_star[%4d] = %20.12f\r\n",
+                    ii, N_part_per_slice[ ii ],
+                    ii, x_slices_star[ ii ],
+                    ii, y_slices_star[ ii ],
+                    ii, sigma_slices_star[ ii ] );
+        }
+    }
+    else
+    {
+        fprintf( fp, "|beambeam6d      | enabled                = %20ld\r\n",
+                 ( long int )0 );
+    }
+
+    return;
+}
+
+void NS(BeamMonitor_fprint)(
+    FILE* SIXTRL_RESTRICT fp,
+    SIXTRL_ARGPTR_DEC const NS(BeamMonitor) *const SIXTRL_RESTRICT monitor )
+{
+    int const is_rolling = NS(BeamMonitor_is_rolling)( monitor ) ? 1 : 0;
+
+    int const attr_cont =
+        NS(BeamMonitor_are_attributes_continous)( monitor ) ? 1 : 0;
+
+    fprintf( fp,
+            "|beam-monitor     | num stores      = %20d \r\n"
+            "                  | start turn      = %20d;\r\n"
+            "                  | skip turns      = %20d;\r\n"
+            "                  | io_address      = %20lu;\r\n"
+            "                  | io store stride = %20lu;\r\n"
+            "                  | rolling         = %20d;\r\n"
+            "                  | cont attributes = %20d;\r\n",
+            ( int )NS(BeamMonitor_get_num_stores)( monitor ),
+            ( int )NS(BeamMonitor_get_start)( monitor ),
+            ( int )NS(BeamMonitor_get_skip)( monitor ),
+            ( unsigned long )NS(BeamMonitor_get_io_address)( monitor ),
+            ( unsigned long )NS(BeamMonitor_get_io_store_stride)( monitor ),
+            is_rolling, attr_cont );
+
+    return;
+}
+
 /* ------------------------------------------------------------------------- */
 
 void NS(BeamElement_fprint)( FILE* SIXTRL_RESTRICT fp,
@@ -249,20 +483,32 @@ void NS(BeamElement_fprint)( FILE* SIXTRL_RESTRICT fp,
                 break;
             }
 
-            /*
-            case NS(BLOCK_TYPE_BEAM_BEAM):
+            case NS(OBJECT_TYPE_BEAM_BEAM_4D):
             {
-                typedef SIXTRL_DATAPTR_DEC NS(BeamBeam) const* ptr_to_belem_t;
-                ptr_to_belem_t belem = ( ptr_to_belem_t )( uintptr_t )begin_addr;
+                typedef SIXTRL_DATAPTR_DEC NS(BeamBeam4D) const* ptr_to_belem_t;
+                ptr_to_belem_t beam_element = ( ptr_to_belem_t )( uintptr_t )addr;
 
-                for( ; index < index_end ; ++index )
-                {
-                    ret |= NS(Track_particle_beam_beam)( p, index, belem );
-                }
-
+                NS(BeamBeam4D_fprint)( fp, beam_element );
                 break;
             }
-            */
+
+            case NS(OBJECT_TYPE_BEAM_BEAM_6D):
+            {
+                typedef SIXTRL_DATAPTR_DEC NS(BeamBeam6D) const* ptr_to_belem_t;
+                ptr_to_belem_t beam_element = ( ptr_to_belem_t )( uintptr_t )addr;
+
+                NS(BeamBeam6D_fprint)( fp, beam_element );
+                break;
+            }
+
+            case NS(OBJECT_TYPE_BEAM_MONITOR):
+            {
+                typedef SIXTRL_DATAPTR_DEC NS(BeamMonitor) const* ptr_to_belem_t;
+                ptr_to_belem_t beam_element = ( ptr_to_belem_t )( uintptr_t )addr;
+
+                NS(BeamMonitor_fprint)( fp, beam_element );
+                break;
+            }
 
             default:
             {
