30c3d291146748e9aeea044b6ffe03f368907ccf
giadarol <giovanni.iadarola@cern.ch>
2018-11-26 18:43:36 +0100

    Working...

--- tests/testdata/generators/generate_track_lhc_no_bb_data.py
+++ tests/testdata/generators/generate_track_lhc_no_bb_data.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import test_data_generation
+
+if __name__ == '__main__':
+    test_data_generation.generate_testdata(pyst_example='lhc_no_bb')
+
+prrrrr
 
 import pickle
 import os
