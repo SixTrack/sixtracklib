e340255d21f31503f0721c5cb41b8c2e8dc0d275
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2019-04-13 08:53:42 +0200

    tests/python: fixes calls to the wrongly spelled TrackJob accessor function has_elem_by_elem_output()

--- tests/python/test_track_job_opencl.py
+++ tests/python/test_track_job_opencl.py
@@ -111,7 +111,7 @@ if __name__ == '__main__':
     assert(job.type_str() == 'opencl')
     assert(job.has_output_buffer())
     assert(job.num_beam_monitors() > 0)
-    assert(job.has_elem_by_elem_outupt())
+    assert(job.has_elem_by_elem_output())
     assert(job.has_beam_monitor_output())
 
     status = job.track_elem_by_elem(until_turn_elem_by_elem)
