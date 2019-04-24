3bd668d8a5f07fcb71fda2f0e4a6a8bcaa40e003
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2019-04-24 17:24:50 +0200

    python/pysixtracklib: fixes syntax error in config_helper.py

--- python/pysixtracklib/config_helper.py
+++ python/pysixtracklib/config_helper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from . import config.SIXTRACKLIB_MODULES
+from .config import SIXTRACKLIB_MODULES
 
 def supports( architecture ):
     return bool( SIXTRACKLIB_MODULES.get( architecture, False ) )
