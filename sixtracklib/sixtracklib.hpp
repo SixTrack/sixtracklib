355761afaed3eb43959a15f50c4e85bcdecbc762
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2019-04-25 13:11:27 +0200

    sixtracklib: sync convenience CXX header with present list of required headers

--- sixtracklib/sixtracklib.hpp
+++ sixtracklib/sixtracklib.hpp
@@ -20,20 +20,12 @@
 #if defined( SIXTRACKLIB_ENABLE_MODULE_SIMD ) && \
            ( SIXTRACKLIB_ENABLE_MODULE_SIMD == 1 )
 
-//     #include "sixtracklib/simd/track.h"
-
 #endif /* defined( SIXTRACKLIB_ENABLE_MODULE_SIMD ) */
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 #if defined( SIXTRACKLIB_ENABLE_MODULE_OPENCL ) && \
            ( SIXTRACKLIB_ENABLE_MODULE_OPENCL == 1 )
-
-//     #include "sixtracklib/opencl/argument.h"
-//     #include "sixtracklib/opencl/context.h"
-
-//     #include "sixtracklib/opencl/buffer.h"
-//     #include "sixtracklib/opencl/ocl_environment.h"
 
 #endif /* defined( SIXTRACKLIB_ENABLE_MODULE_OPENCL ) */
 
@@ -42,8 +34,11 @@
 #if defined( SIXTRACKLIB_ENABLE_MODULE_CUDA ) && \
            ( SIXTRACKLIB_ENABLE_MODULE_CUDA == 1 )
 
-//     #include "sixtracklib/cuda/buffer.h"
-//     #include "sixtracklib/cuda/impl/track_particles_kernel_c_wrapper.h"
+#include "sixtracklib/cuda/internal/argument_base.hpp"
+#include "sixtracklib/cuda/argument.hpp"
+
+#include "sixtracklib/cuda/internal/context_base.hpp"
+#include "sixtracklib/cuda/context.hpp"
 
 #endif /* defined( SIXTRACKLIB_ENABLE_MODULE_OPENCL ) */
 
