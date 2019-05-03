03ad1dd14c8eec6c233427228469a44ea1538c66
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2019-05-03 20:40:20 +0200

    tests/cuda: update the C++ unit-test for the sixtrack::CudaController (not finished yet, wip, but seems to work)

--- tests/sixtracklib/cuda/test_controller_cxx.cpp
+++ tests/sixtracklib/cuda/test_controller_cxx.cpp
@@ -5,6 +5,8 @@
 #include <cstdlib>
 #include <cstring>
 
+#include <cuda_runtime_api.h>
+
 #include <gtest/gtest.h>
 
 #include "sixtracklib/testlib.h"
@@ -12,11 +14,36 @@
 TEST( CXX_CudaControllerTests, MinimalUsageTest )
 {
     namespace st = SIXTRL_CXX_NAMESPACE;
+
     using controller_t = st::CudaController;
+    using node_index_t = controller_t::node_index_t;
+    using node_id_t    = controller_t::node_id_t;
+    using node_info_t  = controller_t::node_info_t;
 
     controller_t controller;
 
     ASSERT_TRUE( controller.archId() == st::ARCHITECTURE_CUDA );
+    ASSERT_TRUE( controller.hasArchStr() );
+    ASSERT_TRUE( !controller.archStr().empty() );
+    ASSERT_TRUE( controller.ptrArchStr() != nullptr );
+
+    ASSERT_TRUE( 0 == std::strcmp( controller.ptrArchStr(),
+                   SIXTRL_ARCHITECTURE_CUDA_STR ) );
+
+    int temp_num_devices = int{ -1 };
+    ::cudaError_t err = ::cudaGetDeviceCount( &temp_num_devices );
+
+    if( ( err == ::cudaSuccess ) && ( temp_num_devices > 0 ) )
+    {
+        node_index_t const cmp_num_devices = static_cast< node_index_t >(
+            temp_num_devices );
+
+        ASSERT_TRUE( cmp_num_devices == controller.numAvailableNodes() );
+
+        controller.printAvailableNodesInfo();
+    }
+
+
     ASSERT_TRUE( controller.readyForSend() );
     ASSERT_TRUE( controller.readyForReceive() );
     ASSERT_TRUE( controller.readyForRemap() );
