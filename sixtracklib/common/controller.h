200c2c932f570b140c2262e7ba30f35e1902a587
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2019-05-10 02:06:12 +0200

    sixtracklib: reflects the move from NS(ControllerOnNodesBase) to NS(NodeControllerBase)

--- sixtracklib/common/controller.h
+++ sixtracklib/common/controller.h
@@ -3,13 +3,15 @@
 
 #if !defined( SIXTRL_NO_INCLUDES )
     #include "sixtracklib/common/definitions.h"
+    #include "sixtracklib/common/control/definitions.h"
+
     #include "sixtracklib/common/control/node_id.hpp"
     #include "sixtracklib/common/control/arch_info.hpp"
     #include "sixtracklib/common/control/arch_base.hpp"
     #include "sixtracklib/common/control/node_info.hpp"
     #include "sixtracklib/common/control/argument_base.hpp"
     #include "sixtracklib/common/control/controller_base.hpp"
-    #include "sixtracklib/common/control/controller_on_nodes_base.hpp"
+    #include "sixtracklib/common/control/node_controller_base.hpp"
 
     #include "sixtracklib/common/control/node_id.h"
     #include "sixtracklib/common/control/arch_info.h"
@@ -17,7 +19,7 @@
     #include "sixtracklib/common/control/node_info.h"
     #include "sixtracklib/common/control/argument_base.h"
     #include "sixtracklib/common/control/controller_base.h"
-    #include "sixtracklib/common/control/controller_on_nodes_base.h"
+    #include "sixtracklib/common/control/node_controller_base.h"
 #endif /* !defined( SIXTRL_NO_INCLUDES ) */
 
 #endif /* SIXTRACKLIB_COMMON_CONTEXT_H__ */
