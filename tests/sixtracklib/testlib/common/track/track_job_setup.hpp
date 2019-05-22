e2865d2efd56e48c06f6e96ee3d0ebc7b7955813
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2019-05-22 16:42:37 +0200

    testlib/common: adds additional verificatin routines for setup track-job unit-tests

--- tests/sixtracklib/testlib/common/track/track_job_setup.hpp
+++ tests/sixtracklib/testlib/common/track/track_job_setup.hpp
@@ -16,13 +16,40 @@ namespace SIXTRL_CXX_NAMESPACE
     namespace tests
     {
         bool TestTrackJob_setup_no_required_output(
-            SIXTRL_CXX_NAMESPACE::TrackJobBaseNew& SIXTRL_RESTRICT_REF track_job,
+            SIXTRL_CXX_NAMESPACE::TrackJobBaseNew
+                const& SIXTRL_RESTRICT_REF track_job,
             SIXTRL_CXX_NAMESPACE::Buffer const& SIXTRL_RESTRICT_REF pbuffer,
             SIXTRL_CXX_NAMESPACE::Buffer::size_type const num_psets,
             SIXTRL_CXX_NAMESPACE::Buffer::size_type const*
                 SIXTRL_RESTRICT pset_indices_begin,
             SIXTRL_CXX_NAMESPACE::Buffer const& SIXTRL_RESTRICT_REF belem_buffer,
-            SIXTRL_CXX_NAMESPACE::Buffer* SIXTRL_RESTRICT ptr_output_buffer );
+            SIXTRL_CXX_NAMESPACE::Buffer const* SIXTRL_RESTRICT ptr_output_buffer );
+
+        bool TestTrackJob_setup_no_beam_monitors_elem_by_elem(
+            SIXTRL_CXX_NAMESPACE::TrackJobBaseNew
+                const& SIXTRL_RESTRICT_REF track_job,
+            SIXTRL_CXX_NAMESPACE::Buffer const& SIXTRL_RESTRICT_REF pbuffer,
+            SIXTRL_CXX_NAMESPACE::Buffer::size_type const num_psets,
+            SIXTRL_CXX_NAMESPACE::Buffer::size_type const*
+                SIXTRL_RESTRICT pset_indices_begin,
+            SIXTRL_CXX_NAMESPACE::Buffer const& SIXTRL_RESTRICT_REF belem_buffer,
+            SIXTRL_CXX_NAMESPACE::Buffer const* SIXTRL_RESTRICT ptr_output_buffer,
+            SIXTRL_CXX_NAMESPACE::Buffer::size_type
+                const until_turn_elem_by_elem );
+
+        bool TestTrackJob_setup_beam_monitors_and_elem_by_elem(
+            SIXTRL_CXX_NAMESPACE::TrackJobBaseNew
+                const& SIXTRL_RESTRICT_REF track_job,
+            SIXTRL_CXX_NAMESPACE::Buffer const& SIXTRL_RESTRICT_REF pbuffer,
+            SIXTRL_CXX_NAMESPACE::Buffer::size_type const num_psets,
+            SIXTRL_CXX_NAMESPACE::Buffer::size_type const* SIXTRL_RESTRICT pset_indices_begin,
+            SIXTRL_CXX_NAMESPACE::Buffer const& SIXTRL_RESTRICT_REF belem_buffer,
+            SIXTRL_CXX_NAMESPACE::Buffer const* SIXTRL_RESTRICT ptr_output_buffer,
+            SIXTRL_CXX_NAMESPACE::Buffer::size_type const num_beam_monitors,
+            SIXTRL_CXX_NAMESPACE::Buffer::size_type const until_turn,
+            SIXTRL_CXX_NAMESPACE::Buffer::size_type const until_turn_elem_by_elem );
+
+
     }
 }
 
