03dddd637f2d5da06b8c1f58dfb7ff60c6438f8a
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2019-06-06 00:06:09 +0200

    tests/common: re-adds missing NS(BeamMonitor) C99 unit-test

--- tests/sixtracklib/common/beam_elements/test_be_monitor_c99.cpp
+++ tests/sixtracklib/common/beam_elements/test_be_monitor_c99.cpp
@@ -943,7 +943,8 @@ TEST( C99_CommonBeamMonitorTests, TrackingAndTurnByTurnIO )
                         ::st_Particles_get_num_of_particles(
                             elem_by_elem_particles ) );
 
-                    ASSERT_TRUE( ::st_Particles_copy_single( particles, ll,
+                    ASSERT_TRUE( ::NS(ARCH_STATUS_SUCCESS) ==
+                    ::st_Particles_copy_single( particles, ll,
                         elem_by_elem_particles, elem_by_elem_index ) );
 
                     num_elem_t const stored_particle_id =
@@ -951,7 +952,8 @@ TEST( C99_CommonBeamMonitorTests, TrackingAndTurnByTurnIO )
                             mon, kk, particle_id );
 
                     ASSERT_TRUE( stored_particle_id >= num_elem_t{ 0 } );
-                    ASSERT_TRUE( ::st_Particles_copy_single( cmp_particles, ll,
+                    ASSERT_TRUE( ::NS(ARCH_STATUS_SUCCESS) ==
+                        ::st_Particles_copy_single( cmp_particles, ll,
                                     out_particles, stored_particle_id ) );
                 }
 
