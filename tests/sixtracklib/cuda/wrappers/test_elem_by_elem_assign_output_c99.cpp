d1f2a32a2f265cc5a5749335bcb5735f76a6ec7e
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2019-06-05 16:00:14 +0200

    tests/cuda: fixes inconsistencies in unit-test for the output assignment elem-by-elem cuda wrapper test

--- tests/sixtracklib/cuda/wrappers/test_elem_by_elem_assign_output_c99.cpp
+++ tests/sixtracklib/cuda/wrappers/test_elem_by_elem_assign_output_c99.cpp
@@ -24,7 +24,7 @@
 #include "sixtracklib/cuda/controller.h"
 #include "sixtracklib/cuda/argument.h"
 
-TEST( C99_CudaWrappersElemByElemAssignOutputTests, 
+TEST( C99_CudaWrappersElemByElemAssignOutputTests,
       TestDefaultElemByElemConfig )
 {
     namespace st = SIXTRL_CXX_NAMESPACE;
@@ -35,9 +35,9 @@ TEST( C99_CudaWrappersElemByElemAssignOutputTests,
 
     using cuda_ctrl_t           = ::NS(CudaController);
     using cuda_arg_t            = ::NS(CudaArgument);
-    using cuda_kernel_conf_t    = ::NS(CudaKernelConfig);    
+    using cuda_kernel_conf_t    = ::NS(CudaKernelConfig);
     using cuda_node_info_t      = ::NS(CudaNodeInfo);
-                                
+
     using size_t                = ::NS(buffer_size_t);
     using pindex_t              = ::NS(particle_index_t);
     using status_t              = ::NS(arch_status_t);
@@ -48,26 +48,26 @@ TEST( C99_CudaWrappersElemByElemAssignOutputTests,
     c_buffer_t* output_buffer        = ::NS(Buffer_new)( size_t{ 0 } );
     c_buffer_t* beam_elements_buffer = ::NS(Buffer_new_from_file)(
         ::NS(PATH_TO_BEAMBEAM_BEAM_ELEMENTS) );
-    
+
     SIXTRL_ASSERT( beam_elements_buffer != nullptr );
-    SIXTRL_ASSERT( ::NS(Buffer_get_num_of_objects)( beam_elements_buffer ) > 
+    SIXTRL_ASSERT( ::NS(Buffer_get_num_of_objects)( beam_elements_buffer ) >
         size_t{ 0 } );
-    
+
     size_t const NUM_PARTICLES = size_t{ 10 };
-    
-    particles_t* particles = ::NS(Particles_new)( 
+
+    particles_t* particles = ::NS(Particles_new)(
         particles_buffer, NUM_PARTICLES );
-    
+
     SIXTRL_ASSERT( particles != nullptr );
-    
+
     /* Init particles  */
-    pindex_t const min_at_turn_id = pindex_t{ 5 };
-    
+    pindex_t const min_at_turn_id = pindex_t{ 0 };
+
     ::NS(Particles_realistic_init)( particles );
     ::NS(Particles_set_all_at_turn_value)( particles, min_at_turn_id );
-    
+
     size_t const UNTIL_TURN_ELEM_BY_ELEM = size_t{ 5 };
-    
+
     /* -------------------------------------------------------------------- */
     /* Init the Cuda controller and arguments for the addresses
      * and the particles */
@@ -107,9 +107,9 @@ TEST( C99_CudaWrappersElemByElemAssignOutputTests,
             ::NS(NodeInfo_print_out)( ptr_node_info );
 
             /* ************************************************************* */
-            
+
             std::string kernel_name = SIXTRL_C99_NAMESPACE_PREFIX_STR;
-            kernel_name += 
+            kernel_name +=
                 "ElemByElemConfig_assign_out_buffer_from_offset_cuda_wrapper";
 
             kernel_id_t const kernel_id =
@@ -125,70 +125,70 @@ TEST( C99_CudaWrappersElemByElemAssignOutputTests,
                 ptr_kernel_config, ptr_node_info );
 
             ASSERT_TRUE( status == ::NS(ARCH_STATUS_SUCCESS) );
-            
+
             /* ************************************************************* */
 
             elem_by_elem_config_t elem_by_elem_conf;
             ::NS(ElemByElemConfig_preset)( &elem_by_elem_conf );
-            
+
             cuda_arg_t* elem_by_elem_conf_arg = ::NS(CudaArgument_new)( ctrl );
             SIXTRL_ASSERT( elem_by_elem_conf_arg != nullptr );
-            
+
             cuda_arg_t* output_arg = ::NS(CudaArgument_new)( ctrl );
             SIXTRL_ASSERT( output_arg != nullptr );
-            
+
             cuda_arg_t* result_arg = ::NS(CudaArgument_new)( ctrl );
             SIXTRL_ASSERT( result_arg != nullptr );
-            
+
             size_t const num_particle_sets = size_t{ 1 };
             size_t const pset_indices_begin[] = { size_t{ 0 } };
-            
-            size_t output_buffer_index_offset = 
+
+            size_t output_buffer_index_offset =
                 ::NS(Buffer_get_num_of_objects)( output_buffer ) + size_t{ 1 };
-            
+
             status = ::NS(TestElemByElemConfigCtrlArg_prepare_assign_output_buffer)(
-                particles_buffer, num_particle_sets, &pset_indices_begin[ 0 ], 
+                particles_buffer, num_particle_sets, &pset_indices_begin[ 0 ],
                 beam_elements_buffer, elem_by_elem_conf_arg, &elem_by_elem_conf,
                 output_arg, output_buffer, &output_buffer_index_offset,
                 UNTIL_TURN_ELEM_BY_ELEM, result_arg );
-            
+
             SIXTRL_ASSERT( status == ::NS(ARCH_STATUS_SUCCESS) );
-            
+
             ::NS(ElemByElemConfig_assign_out_buffer_from_offset_cuda_wrapper)(
-                ptr_kernel_config, elem_by_elem_conf_arg, output_arg, 
+                ptr_kernel_config, elem_by_elem_conf_arg, output_arg,
                     output_buffer_index_offset, result_arg );
-            
+
             status = ::NS(TestElemByElemConfigCtrlArg_evaluate_assign_output_buffer)(
-                elem_by_elem_conf_arg, &elem_by_elem_conf, output_arg,                
+                elem_by_elem_conf_arg, &elem_by_elem_conf, output_arg,
                 output_buffer, output_buffer_index_offset, result_arg );
-            
+
             ASSERT_TRUE( status == NS(ARCH_STATUS_SUCCESS) );
-            
+
             /* ************************************************************* */
-            
+
             ::NS(Argument_delete)( elem_by_elem_conf_arg );
             ::NS(Argument_delete)( output_arg );
             ::NS(Argument_delete)( result_arg );
-            
+
             elem_by_elem_conf_arg = nullptr;
             result_arg = nullptr;
             output_arg = nullptr;
-            
+
             ++num_processed_nodes;
         }
-        
+
         ASSERT_TRUE( num_processed_nodes == static_cast< size_t >(
             num_avail_nodes ) );
     }
-    else 
+    else
     {
         std::cout << "No cuda nodes found -> skipping test" << std::endl;
     }
-    
+
     ::NS(Buffer_delete)( particles_buffer );
     ::NS(Buffer_delete)( output_buffer );
-    ::NS(Buffer_delete)( beam_elements_buffer );    
-    
+    ::NS(Buffer_delete)( beam_elements_buffer );
+
     ::NS(Controller_delete)( ctrl );
 }
 
