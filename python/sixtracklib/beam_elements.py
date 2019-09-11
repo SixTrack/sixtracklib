57ddeffb7abcaf6808c51a0fb790acc3bc103fca
Adrian Oeftiger <a.oeftiger@gsi.de>
2019-09-11 15:54:09 +0200

    beam_elements.py: fixing from_line method
    which is a class method and did not return an instance
    (the append_line method does not return self and probably
    should not, therefore instantiate the class inside from_line,
    append the provided line arg, and then return the instance)

--- python/sixtracklib/beam_elements.py
+++ python/sixtracklib/beam_elements.py
@@ -446,7 +446,9 @@ class Elements(object):
 
     @classmethod
     def from_line(cls, line):
-        return cls().append_line(line)
+        self = cls()
+        self.append_line(line)
+        return self
 
     def append_line(self, line):
         for element in line.elements:
