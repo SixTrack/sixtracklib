f274c91b15a44c04bf2245d98eb061b1f9f9782e
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2019-11-09 00:26:23 +0100

    cmake: adds Python 3.8 to the list of supported versions

--- cmake/SetupPython.cmake
+++ cmake/SetupPython.cmake
@@ -17,7 +17,7 @@ if(  NOT SETUP_PYTHON_FINISHED )
     # ==========================================================================
 
     if( NOT  Python_ADDITIONAL_VERSIONS )
-        set( Python_ADDITIONAL_VERSIONS 3.7 3.6 3 )
+        set( Python_ADDITIONAL_VERSIONS 3.8 3.7 3.6 3 )
     endif()
 
     if( SIXTRACKL_ENABLE_PYTHON AND NOT PYTHONINTERP_FOUND )
