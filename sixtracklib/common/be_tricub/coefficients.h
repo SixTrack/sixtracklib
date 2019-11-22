6dc3e5ce283cf5c2c0b94bb7e190bce5a2116e41
Martin Schwinzerl <martin.schwinzerl@lefou.at>
2019-11-22 10:28:50 +0100

    common: adds a stub version of the be_tricub/coefficients.h file

---
+++ sixtracklib/common/be_tricub/coefficients.h
@@ -0,0 +1,43 @@
+#ifndef SIXTRACKLIB_COMMON_BE_TRICUB_COEFFICIENTS_C99_H__
+#define SIXTRACKLIB_COMMON_BE_TRICUB_COEFFICIENTS_C99_H__
+
+/*
+#if !defined(SIXTRL_NO_SYSTEM_INCLUDES)
+    #include <stddef.h>
+    #include <math.h>
+#endif */
+/* !defined(SIXTRL_NO_SYSTEM_INCLUDES) */
+
+#if !defined(SIXTRL_NO_INCLUDES)
+    #include "sixtracklib/common/definitions.h"
+    #include "sixtracklib/common/be_tricub/be_tricub.h"
+#endif /* !defined(SIXTRL_NO_INCLUDES) */
+
+#if !defined( _GPUCODE ) && defined( __cplusplus )
+extern "C" {
+#endif /* !defined( _GPUCODE ) && defined( __cplusplus ) */
+
+/* SUGGESTION: Up here only the signatures / declarations */
+
+#if !defined( _GPUCODE ) && defined( __cplusplus )
+}
+#endif /* !defined( _GPUCODE ) && defined( __cplusplus ) */
+
+/* ************************************************************************* */
+/* ************************************************************************* */
+
+#if !defined( _GPUCODE ) && defined( __cplusplus )
+extern "C" {
+#endif /* !defined( _GPUCODE ) && defined( __cplusplus ) */
+
+/* SUGGESTION: Up here the inline declrations
+    INLINE: SIXTRL_INLINE and SIXTRL_STATIC SIXTRL_FN above
+ */
+
+#if !defined( _GPUCODE ) && defined( __cplusplus )
+}
+#endif /* !defined( _GPUCODE ) && defined( __cplusplus ) */
+
+#endif /* SIXTRACKLIB_COMMON_BE_TRICUB_COEFFICIENTS_C99_H__ */
+
+
