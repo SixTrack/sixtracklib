14869350b8be8eb79fcb5737a10b4e6444c02763
Martin Schwinzerl <martin.schwinzerl@lefou.at>
2019-12-02 22:52:30 +0100

    tests/python: adds checks to all cuda unit-tests to skip if no cuda nodes are available

--- tests/python/cuda/test_cuda_controller.py
+++ tests/python/cuda/test_cuda_controller.py
@@ -4,16 +4,25 @@
 import sys
 import os
 from cobjects import CBuffer
-import sixtracklib as pyst
+import sixtracklib as st
 from sixtracklib.stcommon import st_NODE_UNDEFINED_INDEX
 import sixtracklib_test as testlib
 from sixtracklib_test.generic_obj import GenericObj
 
 if __name__ == '__main__':
-    if not pyst.supports('cuda'):
+    if not st.supports('cuda'):
         raise SystemExit("cuda support required for this test")
 
-    ctrl = pyst.CudaController()
+    try:
+        num_nodes = st.CudaController.NUM_AVAILABLE_NODES()
+    except RuntimeError as e:
+        num_nodes = 0
+
+    if num_nodes <= 0:
+        print("No CUDA nodes available -> skip test")
+        sys.exit(0)
+
+    ctrl = st.CudaController()
     assert ctrl.num_nodes > 0
     assert ctrl.has_selected_node
     assert ctrl.selected_node_index != st_NODE_UNDEFINED_INDEX.value
@@ -24,14 +33,14 @@ if __name__ == '__main__':
 
     selected_node_id = ctrl.selected_node_id
 
-    assert isinstance(selected_node_id, pyst.NodeId)
+    assert isinstance(selected_node_id, st.NodeId)
     assert selected_node_id.is_valid
 
     node_id_str = str(selected_node_id)
     print("selected node id: {0}".format(selected_node_id))
 
     selected_node_info = ctrl.selected_node_info
-    assert isinstance(selected_node_info, pyst.NodeInfoBase)
+    assert isinstance(selected_node_info, st.NodeInfoBase)
 
     print("selected node_info: ")
     print(selected_node_info)
