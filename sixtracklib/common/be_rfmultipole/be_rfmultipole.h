3e51e75c0fc086fba7980cf379cff28d56f673d4
Martin Schwinzerl <martin.schwinzerl@lefou.at>
2019-12-09 10:23:58 +0100

    common: adds stub for RFMultiPole beam element

--- sixtracklib/common/be_rfmultipole/be_rfmultipole.h
+++ sixtracklib/common/be_rfmultipole/be_rfmultipole.h
@@ -1,5 +1,294 @@
 #ifndef SIXTRACKL_COMMON_BE_RFMULTIPOLE_BE_RFMULTIPOLE_C99_H__
 #define SIXTRACKL_COMMON_BE_RFMULTIPOLE_BE_RFMULTIPOLE_C99_H__
 
+#if !defined( SIXTRL_NO_INCLUDES )
+    #include "sixtracklib/common/definitions.h"
+    #include "sixtracklib/common/control/definitions.h"
+    #include "sixtracklib/common/buffer/buffer_type.h"
+    #include "sixtracklib/common/buffer/buffer_object.h"
+    #include "sixtracklib/common/buffer/object_type_id.h"
+#endif /* !defined( SIXTRL_NO_INCLUDES ) */
+
+#if !defined( SIXTRL_NO_SYSTEM_INCLUDES )
+    #include <stddef.h>
+    #include <stdlib.h>
+    #include <stdint.h>
+#endif /* !defined( SIXTRL_NO_SYSTEM_INCLUDES ) */
+
+#if defined( __cplusplus ) && !defined( _GPUCODE )
+extern "C" {
+#endif /* defined( __cplusplus ) && !defined( _GPUCODE ) */
+
+typedef SIXTRL_REAL_T  NS(rf_multipole_real_t);
+typedef SIXTRL_INT64_T NS(rf_multipole_int_t);
+
+typedef struct NS(RFMultiPole)
+{
+    NS(rf_multipole_int_t)    order               SIXTRL_ALIGN( 8 );
+    NS(rf_multipole_real_t)   voltage             SIXTRL_ALIGN( 8 );
+    NS(rf_multipole_real_t)   frequency           SIXTRL_ALIGN( 8 );
+    NS(rf_multipole_real_t)   lag                 SIXTRL_ALIGN( 8 );
+    NS(buffer_addr_t)         bal                 SIXTRL_ALIGN( 8 );
+    NS(buffer_addr_t)         p                   SIXTRL_ALIGN( 8 );
+}
+NS(RFMultiPole);
+
+SIXTRL_STATIC SIXTRL_FN SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)*
+NS(RFMultiPole_preset)(
+    SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)* SIXTRL_RESTRICT multipole );
+
+SIXTRL_STATIC SIXTRL_FN void NS(RFMultiPole_clear)(
+    SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)* SIXTRL_RESTRICT multipole );
+
+/* ------------------------------------------------------------------- */
+
+SIXTRL_STATIC SIXTRL_FN NS(object_type_id_t) NS(RFMultiPole_type_id)(
+    SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT mpole );
+
+SIXTRL_STATIC SIXTRL_FN NS(buffer_size_t) NS(RFMultiPole_num_dataptrs)(
+    SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT mpole );
+
+SIXTRL_STATIC SIXTRL_FN NS(buffer_size_t) NS(RFMultiPole_num_slots)(
+    SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT mpole,
+    NS(buffer_size_t) const slot_size );
+
+/* ------------------------------------------------------------------- */
+
+SIXTRL_STATIC SIXTRL_FN SIXTRL_BE_ARGPTR_DEC NS(rf_multipole_real_t) const*
+NS(RFMultiPole_const_bal)( SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole)
+    *const SIXTRL_RESTRICT mpole );
+
+SIXTRL_STATIC SIXTRL_FN SIXTRL_BE_ARGPTR_DEC NS(rf_multipole_real_t) const*
+NS(RFMultiPole_bal)( SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)* SIXTRL_RESTRICT mpole );
+
+SIXTRL_STATIC SIXTRL_FN SIXTRL_BE_ARGPTR_DEC NS(rf_multipole_real_t) const*
+NS(RFMultiPole_const_p)( SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole)
+    *const SIXTRL_RESTRICT mpole );
+
+SIXTRL_STATIC SIXTRL_FN SIXTRL_BE_ARGPTR_DEC NS(rf_multipole_real_t) const*
+NS(RFMultiPole_p)( SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)* SIXTRL_RESTRICT mpole );
+
+/* ------------------------------------------------------------------- */
+
+SIXTRL_STATIC SIXTRL_FN void NS(RFMultiPole_set_bal_addr)(
+    SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT mpole,
+    NS(buffer_addr_t) const bal_addr );
+
+SIXTRL_STATIC SIXTRL_FN void NS(RFMultiPole_set_p_addr)(
+    SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT mpole,
+    NS(buffer_addr_t) const p_addr );
+
+/* ------------------------------------------------------------------- */
+
+SIXTRL_STATIC SIXTRL_FN SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole) const*
+NS(RFMultiPole_const_from_obj_index)(
+    SIXTRL_BUFFER_OBJ_ARGPTR_DEC const NS(Object) *const obj );
+
+SIXTRL_STATIC SIXTRL_FN SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)*
+NS(RFMultiPole_from_obj_index)( SIXTRL_BUFFER_OBJ_ARGPTR_DEC NS(Object)* obj );
+
+SIXTRL_STATIC SIXTRL_FN SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole) const*
+NS(RFMultiPole_const_from_managed_buffer)(
+    SIXTRL_BUFFER_DATAPTR_DEC unsigned char const* SIXTRL_RESTRICT buffer_begin,
+    NS(buffer_size_t) const index, NS(buffer_size_t) const slot_size );
+
+SIXTRL_STATIC SIXTRL_FN SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)*
+NS(RFMultiPole_from_managed_buffer)(
+    SIXTRL_BUFFER_DATAPTR_DEC unsigned char* SIXTRL_RESTRICT buffer_begin,
+    NS(buffer_size_t) const index, NS(buffer_size_t) const slot_size );
+
+#if !defined( _GPUCODE )
+
+SIXTRL_EXTERN SIXTRL_HOST_FN SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole) const*
+NS(RFMultiPole_const_from_buffer)(
+    SIXTRL_BUFFER_ARGPTR_DEC const NS(Buffer) *const SIXTRL_RESTRICT buffer,
+    NS(buffer_size_t) const index );
+
+SIXTRL_EXTERN SIXTRL_HOST_FN SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)*
+NS(RFMultiPole_from_buffer)(
+    SIXTRL_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer,
+    NS(buffer_size_t) const index );
+
+SIXTRL_EXTERN SIXTRL_HOST_FN NS(arch_status_t) NS(RFMultiPole_attributes_offsets)(
+    SIXTRL_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT offsets_begin,
+    NS(buffer_size_t) const max_num_offsets,
+    SIXTRL_BUFFER_DATAPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT data,
+    NS(buffer_size_t) const slot_size );
+
+SIXTRL_EXTERN SIXTRL_HOST_FN NS(arch_status_t) NS(RFMultiPole_attributes_sizes)(
+    SIXTRL_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT sizes_begin,
+    NS(buffer_size_t) const max_num_sizes,
+    SIXTRL_BUFFER_DATAPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT data,
+    NS(buffer_size_t) const slot_size );
+
+SIXTRL_EXTERN SIXTRL_HOST_FN NS(arch_status_t) NS(RFMultiPole_attributes_counts)(
+    SIXTRL_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT counts_begin,
+    NS(buffer_size_t) const max_num_counts,
+    SIXTRL_BUFFER_DATAPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT data );
+
+/* ------------------------------------------------------------------------- */
+
+SIXTRL_EXTERN SIXTRL_HOST_FN bool NS(RFMultiPole_can_be_added)(
+    SIXTRL_BUFFER_ARGPTR_DEC const NS(Buffer) *const SIXTRL_RESTRICT buffer,
+    NS(rf_multipole_int_t) const order,
+    SIXTRL_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT ptr_requ_objects,
+    SIXTRL_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT ptr_requ_slots,
+    SIXTRL_ARGPTR_DEC NS(buffer_size_t)* SIXTRL_RESTRICT ptr_requ_dataptrs );
+
+SIXTRL_EXTERN SIXTRL_HOST_FN SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)*
+NS(RFMultiPole_new)(
+    SIXTRL_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer,
+    NS(rf_multipole_int_t) const order );
+
+SIXTRL_EXTERN SIXTRL_HOST_FN SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)*
+NS(RFMultiPole_add)(
+    SIXTRL_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer,
+    NS(rf_multipole_int_t) const order,
+    NS(rf_multipole_real_t) const voltage,
+    NS(rf_multipole_real_t) const frequency,
+    NS(rf_multipole_real_t) const lag,
+    SIXTRL_ARGPTR_DEC NS(rf_multipole_real_t) const* SIXTRL_RESTRICT bal_values,
+    SIXTRL_ARGPTR_DEC NS(rf_multipole_real_t) const* SIXTRL_RESTRICT p_values );
+
+SIXTRL_EXTERN SIXTRL_HOST_FN SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)*
+NS(RFMultiPole_add_copy)(
+    SIXTRL_BUFFER_ARGPTR_DEC NS(Buffer)* SIXTRL_RESTRICT buffer,
+    SIXTRL_BUFFER_ARGPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT mpole );
+
+#endif /* !defined( _GPUCODE ) */
+
+SIXTRL_STATIC SIXTRL_FN NS(arch_status_t) NS(RFMultiPole_copy)(
+    SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)* SIXTRL_RESTRICT dest,
+    SIXTRL_BUFFER_DATAPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT src );
+
+#if defined( __cplusplus ) && !defined( _GPUCODE )
+}
+#endif /* defined( __cplusplus ) && !defined( _GPUCODE ) */
+
+/* ****************************************************************** */
+
+#if defined( __cplusplus ) && !defined( _GPUCODE )
+extern "C" {
+#endif /* defined( __cplusplus ) && !defined( _GPUCODE ) */
+
+SIXTRL_INLINE SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)*
+NS(RFMultiPole_preset)(
+    SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)* SIXTRL_RESTRICT multipole )
+{
+
+}
+
+SIXTRL_INLINE void NS(RFMultiPole_clear)(
+    SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)* SIXTRL_RESTRICT multipole )
+{
+
+}
+
+/* ----------------------------------------------------------------------- */
+
+SIXTRL_INLINE NS(object_type_id_t) NS(RFMultiPole_type_id)(
+    SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT mpole )
+{
+
+}
+
+SIXTRL_INLINE NS(buffer_size_t) NS(RFMultiPole_num_dataptrs)(
+    SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT mpole )
+{
+
+}
+
+SIXTRL_INLINE NS(buffer_size_t) NS(RFMultiPole_num_slots)(
+    SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT mpole,
+    NS(buffer_size_t) const slot_size )
+{
+
+}
+
+/* ------------------------------------------------------------------- */
+
+SIXTRL_INLINE SIXTRL_BE_ARGPTR_DEC NS(rf_multipole_real_t) const*
+NS(RFMultiPole_const_bal)( SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole)
+    *const SIXTRL_RESTRICT mpole )
+{
+
+}
+
+SIXTRL_INLINE SIXTRL_BE_ARGPTR_DEC NS(rf_multipole_real_t) const*
+NS(RFMultiPole_bal)( SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)* SIXTRL_RESTRICT mpole )
+{
+
+}
+
+SIXTRL_INLINE SIXTRL_BE_ARGPTR_DEC NS(rf_multipole_real_t) const*
+NS(RFMultiPole_const_p)( SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole)
+    *const SIXTRL_RESTRICT mpole )
+{
+
+}
+
+SIXTRL_INLINE SIXTRL_BE_ARGPTR_DEC NS(rf_multipole_real_t) const*
+NS(RFMultiPole_p)( SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)* SIXTRL_RESTRICT mpole )
+{
+
+}
+
+/* ------------------------------------------------------------------- */
+
+SIXTRL_INLINE void NS(RFMultiPole_set_bal_addr)(
+    SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT mpole,
+    NS(buffer_addr_t) const bal_addr )
+{
+
+}
+
+SIXTRL_INLINE void NS(RFMultiPole_set_p_addr)(
+    SIXTRL_BE_ARGPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT mpole,
+    NS(buffer_addr_t) const p_addr )
+{
+
+}
+
+/* ------------------------------------------------------------------- */
+
+SIXTRL_INLINE SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole) const*
+NS(RFMultiPole_const_from_obj_index)(
+    SIXTRL_BUFFER_OBJ_ARGPTR_DEC const NS(Object) *const obj )
+{
+
+}
+
+SIXTRL_INLINE SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)*
+NS(RFMultiPole_from_obj_index)( SIXTRL_BUFFER_OBJ_ARGPTR_DEC NS(Object)* obj )
+{
+
+}
+
+SIXTRL_INLINE SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole) const*
+NS(RFMultiPole_const_from_managed_buffer)(
+    SIXTRL_BUFFER_DATAPTR_DEC unsigned char const* SIXTRL_RESTRICT buffer_begin,
+    NS(buffer_size_t) const index, NS(buffer_size_t) const slot_size )
+{
+
+}
+
+SIXTRL_INLINE SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)*
+NS(RFMultiPole_from_managed_buffer)(
+    SIXTRL_BUFFER_DATAPTR_DEC unsigned char* SIXTRL_RESTRICT buffer_begin,
+    NS(buffer_size_t) const index, NS(buffer_size_t) const slot_size )
+{
+
+}
+
+SIXTRL_INLINE NS(arch_status_t) NS(RFMultiPole_copy)(
+    SIXTRL_BE_ARGPTR_DEC NS(RFMultiPole)* SIXTRL_RESTRICT dest,
+    SIXTRL_BUFFER_DATAPTR_DEC const NS(RFMultiPole) *const SIXTRL_RESTRICT src )
+{
+
+}
+
+#if defined( __cplusplus ) && !defined( _GPUCODE )
+}
+#endif /* defined( __cplusplus ) && !defined( _GPUCODE ) */
 
 #endif /* SIXTRACKL_COMMON_BE_RFMULTIPOLE_BE_RFMULTIPOLE_C99_H__ */
