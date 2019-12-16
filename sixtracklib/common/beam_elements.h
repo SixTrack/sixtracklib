3904326e62392d5697b3146a6754465d159a8a22
Konstantinos Paraschou <konstantinos.paraschou@cern.ch>
2019-12-16 13:04:45 +0100

    common: added rf multipole in beam elements.h

--- sixtracklib/common/beam_elements.h
+++ sixtracklib/common/beam_elements.h
@@ -150,6 +150,7 @@ SIXTRL_INLINE bool NS(BeamElements_is_beam_element_obj)(
             case NS(OBJECT_TYPE_LIMIT_RECT):
             case NS(OBJECT_TYPE_LIMIT_ELLIPSE):
             case NS(OBJECT_TYPE_DIPEDGE):
+            case NS(OBJECT_TYPE_RF_MULTIPOLE):
             {
                 is_beam_element = true;
                 break;
