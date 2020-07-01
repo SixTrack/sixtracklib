ccbb1cc109816a6f121abfcd5dd4f1068b02a33e
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-07-01 03:59:50 +0200

    python/sixtracklib: fixes interpol.py implementation
    - fixes type error for casting the base address of the CObjects instance
      to a CTypes pointer
    - updates the constructor to handle complex scenarios with capacity/num_values
    - provides a convenience function for mapping NS(LineProfileDensityData) to
      NS(SpaceChargeInterpolatedProfile) instances
    
    TODO: Consider expanding the constructor to directly handle values and
          derivatives during the construction of an instance

--- python/sixtracklib/interpol.py
+++ python/sixtracklib/interpol.py
@@ -4,9 +4,19 @@
 import numpy as np
 import ctypes as ct
 from cobjects import CBuffer, CObject, CField
-from .stcommon import st_ARCH_STATUS_SUCCESS, st_LineDensityProfileData_p, \
+
+from .trackjob import TrackJob
+from .beam_elements import Elements, SpaceChargeInterpolatedProfile
+from .buffer import AssignAddressItem, get_cbuffer_from_obj, Buffer
+from .stcommon import st_ARCH_STATUS_SUCCESS, \
+    st_ARCH_ILLEGAL_BUFFER_ID, \
+    st_ARCH_BEAM_ELEMENTS_BUFFER_ID, \
+    st_LineDensityProfileData_p, \
     st_LineDensityProfileData_type_id, \
     st_NullLineDensityProfileData, \
+    st_NullSpaceChargeInterpolatedProfile, \
+    st_SpaceChargeInterpolatedProfile_type_id, \
+    st_SpaceChargeInterpolatedProfile_interpol_data_addr_offset, \
     st_LineDensityProfileData_values_offset, \
     st_LineDensityProfileData_derivatives_offset, \
     st_LineDensityProfileData_prepare_interpolation, \
@@ -21,83 +31,103 @@ class LineDensityProfileData(CObject):
     INTERPOL_CUBIC = 1
     INTERPOL_NONE = 255
 
-    method = CField(0, "uint64", default=INTERPOL_LINEAR, alignment=8)
-    num_values = CField(1, "int64", default=0, alignment=8)
-    values = CField(2, "real", default=0.0, pointer=True,
-                    length="capacity", alignment=8)
-    derivatives = CField(3, "real", default=0.0, pointer=True,
-                    length="capacity", alignment=8)
-    z0 = CField(4, "real", default=0.0, alignment=8)
-    dz = CField(5, "real", default=1.0, alignment=8)
-    capacity = CField(6, "int64", const=True, alignment=8, default=1)
+    method = CField(0, "uint64", default=0)
+    capacity = CField(1, "int64", const=True, default=2)
+    values = CField(2, "real", default=0.0, pointer=True, length="capacity")
+    derivatives = CField(3, "real", default=0.0, pointer=True, length="capacity")
+    z0 = CField(4, "real", default=0.0)
+    dz = CField(5, "real", default=1.0)
+    num_values = CField(6, "int64", default=0)
 
-    def __init__(self, **kwargs):
-        if "method" in kwargs and isinstance(kwargs["method"], str ):
-            method_str = kwargs["method"].lower().strip()
+    def __init__(self, method=None, capacity=None, num_values=None, **kwargs):
+        if method is not None and isinstance(method, str ):
+            method_str = method.lower().strip()
             if method_str == "linear":
-                kwargs["method"] = self.INTERPOL_LINEAR
+                method = self.INTERPOL_LINEAR
             elif method_str == "cubic":
-                kwargs["method"] = self.INTERPOL_CUBIC
+                method = self.INTERPOL_CUBIC
             else:
-                kwargs["method"] = self.INTERPOL_NONE
+                method = self.INTERPOL_NONE
+            kwargs["method"]=method
 
-        if "values" in kwargs and kwargs[ "values" ] is not None:
-            try:
-                temp_num_values = len( kwargs["values"] )
-            except TypeError:
-                temp_num_values = 0
-            if "num_values" not in kwargs or \
-                temp_num_values > kwargs[ "num_values" ]:
-                kwargs[ "num_values" ] = temp_num_values
+        if capacity is not None or num_values is not None:
+            if capacity is None:
+                capacity = 1
+            if num_values is None:
+                num_values = capacity
+            if num_values > capacity:
+                capacity = num_values
 
-        if "num_values" in kwargs and "capacity" in kwargs and \
-            kwargs[ "num_values" ] > kwargs[ "capacity" ]:
-            kwargs[ "capacity" ] = kwargs[ "num_values" ]
-        elif "capacity" in kwargs and "num_values" in kwargs and \
-            kwargs[ "capacity" ] > 0 and kwargs[ "num_values" ] == 0:
-            kwargs[ "num_values" ] = kwargs[ "capacity" ]
+            kwargs["capacity"] = capacity
+            kwargs["num_values"] = num_values
 
-        if "capacity" in kwargs and kwargs[ "capacity" ] and \
-            "method" in kwargs and kwargs["method"] == self.INTERPOL_NONE and \
-            kwargs[ "capacity" ] < 2:
-            raise ValueError( "linear interpol. requires >= 2 values" )
-        elif "capacity" in kwargs and kwargs[ "capacity" ] and \
-            "method" in kwargs and kwargs["method"] == self.INTERPOL_CUBIC \
-            and kwargs[ "capacity" ] < 4:
-            raise ValueError( "cubic spline interpol. requires >= 4 values" )
-
-        if "dz" in kwargs and kwargs["dz"] <= 0.0:
-            raise ValueError( "dz is required to be > 0" )
-        print( kwargs )
         super().__init__(**kwargs)
+
+    @staticmethod
+    def ptr_offset():
+        return 0
 
     @property
     def values_offset(self):
-        _ptr = ct.cast( self._get_address(), st_LineDensityProfileData_p )
+        _ptr = ct.cast( int( self._get_address() ), st_LineDensityProfileData_p )
         return st_LineDensityProfileData_values_offset( _ptr )
 
     @property
     def derivatives_offset(self):
-        _ptr = ct.cast( self._get_address(), st_LineDensityProfileData_p )
+        _ptr = ct.cast( int( self._get_address() ), st_LineDensityProfileData_p )
         return st_LineDensityProfileData_derivatives_offset( _ptr )
 
     def prepare_interpolation(self):
-        _ptr = ct.cast( self._get_address(), st_LineDensityProfileData_p )
+        _ptr = ct.cast( int( self._get_address() ), st_LineDensityProfileData_p )
         if st_ARCH_STATUS_SUCCESS.value != \
             st_LineDensityProfileData_prepare_interpolation( _ptr ):
             raise RuntimeError( "unable to calculate derivaties for interpolation" )
 
     def interpol(self, z):
-        _ptr = ct.cast( self._get_address(), st_LineDensityProfileData_p )
+        _ptr = ct.cast( int( self._get_address() ), st_LineDensityProfileData_p )
         return st_LineDensityProfileData_interpolate_value(
             _ptr, ct.c_double( z ) )
 
     def interpol_1st_deriv(self, z):
-        _ptr = ct.cast( self._get_address(), st_LineDensityProfileData_p )
+        _ptr = ct.cast( int( self._get_address() ), st_LineDensityProfileData_p )
         return st_LineDensityProfileData_interpolate_1st_derivative(
             _ptr, ct.c_double( z ) )
 
     def interpol_2nd_deriv(self, z):
-        _ptr = ct.cast( self._get_address(), st_LineDensityProfileData_p )
+        _ptr = ct.cast( int( self._get_address() ), st_LineDensityProfileData_p )
         return st_LineDensityProfileData_interpolate_2nd_derivative(
             _ptr, ct.c_double( z ) )
+
+
+def LineDensityProfileData_buffer_create_assign_address_item(
+    track_job, be_sc_index, interpol_buffer_id, lp_data_index ):
+    assert isinstance(track_job, TrackJob)
+    assert interpol_buffer_id != st_ARCH_ILLEGAL_BUFFER_ID.value
+    assert track_job.min_stored_buffer_id <= interpol_buffer_id
+    assert track_job.max_stored_buffer_id >= interpol_buffer_id
+    assert (
+        track_job.stored_buffer(interpol_buffer_id).num_objects
+        > lp_data_index
+    )
+
+    dest_buffer_id = st_ARCH_BEAM_ELEMENTS_BUFFER_ID.value
+    src_buffer_id = interpol_buffer_id
+
+    prev_num_assign_items = track_job.num_assign_items(
+        dest_buffer_id=dest_buffer_id, src_buffer_id=src_buffer_id
+    )
+
+    _ptr_item = track_job.add_assign_address_item(
+        dest_elem_type_id=st_SpaceChargeInterpolatedProfile_type_id(),
+        dest_buffer_id=dest_buffer_id,
+        dest_elem_index=be_sc_index,
+        dest_pointer_offset= st_SpaceChargeInterpolatedProfile_interpol_data_addr_offset(
+            st_NullSpaceChargeInterpolatedProfile ),
+        src_elem_type_id=st_LineDensityProfileData_type_id(),
+        src_buffer_id=src_buffer_id,
+        src_elem_index=lp_data_index,
+        src_pointer_offset=LineDensityProfileData.ptr_offset(),
+    )
+
+    return bool( prev_num_assign_items < track_job.num_assign_items(
+        dest_buffer_id=dest_buffer_id, src_buffer_id=src_buffer_id ) )
