4c7c6b831dea3ffd712abe93899431b3aec09e99
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2021-09-05 15:13:34 +0200

    tests/python: adds unit-test for fixed cernlib implementation

--- tests/python/beam_elements/beamfields/test_cernlib.py
+++ tests/python/beam_elements/beamfields/test_cernlib.py
@@ -7,7 +7,8 @@ from scipy.special import wofz as wofz_scipy
 from sixtracklib.stcommon import \
     st_cerrf_cernlib_c_baseline_q1, \
     st_cerrf_cernlib_c_upstream_q1, \
-    st_cerrf_cernlib_c_optimised_q1
+    st_cerrf_cernlib_c_optimised_q1, \
+    st_cerrf_cernlib_c_optimised_fixed_q1
 
 if __name__ == '__main__':
     x_oe = np.logspace( -8, 8, 101, dtype=np.float64 )
@@ -70,7 +71,7 @@ if __name__ == '__main__':
     assert np.fabs( wz_im_cmp - wz_im_cernlib_upstream ).max() < 1e-10
 
     # --------------------------------------------------------------------------
-    # CERNLib C upstream
+    # CERNLib C optimised
 
     wz_re_cernlib_optimised = np.arange(
         n_im * n_re, dtype=np.float64 ).reshape( n_im, n_re )
@@ -90,5 +91,28 @@ if __name__ == '__main__':
 
     assert np.fabs( wz_re_cmp - wz_re_cernlib_optimised ).max() < 1e-10
     assert np.fabs( wz_im_cmp - wz_im_cernlib_optimised ).max() < 1e-10
+
+    # --------------------------------------------------------------------------
+    # CERNLib C optimised fixed
+
+    wz_re_cernlib_optimised_fixed = np.arange(
+        n_im * n_re, dtype=np.float64 ).reshape( n_im, n_re )
+
+    wz_im_cernlib_optimised_fixed = np.arange(
+        n_im * n_re, dtype=np.float64 ).reshape( n_im, n_re )
+
+    out_re = ct.c_double( 0. )
+    out_im = ct.c_double( 0. )
+
+    for jj, y in enumerate( y_oe ):
+        for ii, x in enumerate( x_oe ):
+            st_cerrf_cernlib_c_optimised_fixed_q1(
+                ct.c_double( x ), ct.c_double( y ),
+                ct.byref( out_re ), ct.byref( out_im ) )
+            wz_re_cernlib_optimised_fixed[ jj, ii ] = np.float64( out_re )
+            wz_im_cernlib_optimised_fixed[ jj, ii ] = np.float64( out_im )
+
+    assert np.fabs( wz_re_cmp - wz_re_cernlib_optimised_fixed ).max() < 1e-10
+    assert np.fabs( wz_im_cmp - wz_im_cernlib_optimised_fixed ).max() < 1e-10
 
 
